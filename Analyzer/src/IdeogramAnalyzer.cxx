e86dfc160474cc341568bd5dba7e84dbdad75bcb
Markus <devnull@localhost>
2011-03-21 00:01:42 +0100

    Ideogram: Narrowed binning, some minor changes...

--- Analyzer/src/IdeogramAnalyzer.cxx
+++ Analyzer/src/IdeogramAnalyzer.cxx
@@ -12,12 +12,12 @@ void IdeogramAnalyzer::Analyze(TString cuts, int i, int j) {
   // S e t u p   c o m p o n e n t   p d f s 
   // ---------------------------------------
 
-  int firstbin = 100;
-  int lastbin  = 250;
+  int firstbin = 150;
+  int lastbin  = 200;
 
   int bins = 50;
 
-  TF1* combiLikelihood = new TF1("combiLikelihood",
+  TF1* combLikelihood = new TF1("combLikelihood",
     "[2] * 0.7 * (TMath::Gaus([0],x,[1],1) + 0.3 * 1/(3.5+1) * (3.5 * TMath::Gaus([0], (6.329e+01 + [0]*0.6317), 25, 1) + TMath::Gaus([0], 225, 46, 1)))");
   TF1* fitParabola = new TF1("fitParabola", "abs([1])*(x-[0])^2+[2]");
   fitParabola->SetParLimits(0, firstbin, lastbin);
@@ -28,8 +28,8 @@ void IdeogramAnalyzer::Analyze(TString cuts, int i, int j) {
 
   TH1F* eventLikelihood = new TH1F("eventLikelihood","eventLikelihood", bins, firstbin, lastbin);
   TH1F* logEventLikelihood = new TH1F("logEventLikelihood", "logEventLikelihood", bins, firstbin, lastbin);
-  TH1F* logSumLikelihood = new TH1F("logSumLikelihood", "logSumLikelihood", bins, firstbin, lastbin);
-  logSumLikelihood->Eval(null);
+  TH1F* sumLogLikelihood = new TH1F("sumLogLikelihood", "sumLogLikelihood", bins, firstbin, lastbin);
+  sumLogLikelihood->Eval(null);
 
   double hadTopMass, fitChi2, fitProb, weight, eventLikelihoodWeight;
   fChain->SetBranchAddress("hadTopMass", &hadTopMass);
@@ -52,13 +52,11 @@ void IdeogramAnalyzer::Analyze(TString cuts, int i, int j) {
       for (int icombi = 0; icombi < 12; icombi++) {
 	      eventTree->GetEntry(ievent + icombi);
 	      
-	      if (icombi!=0 && combi==0) break;
+	      if (icombi!=0 && combi==0 || fitProb < 0.05) break;
 	      
-	      if (fitProb > 0.05) {
-	        weight = fitProb;
-	        combiLikelihood->SetParameters(hadTopMass,12,weight);
-	        eventLikelihood->Eval(combiLikelihood, "A"); // add combi pdf
-	      }
+        weight = fitProb;
+	      combLikelihood->SetParameters(hadTopMass,12,weight);
+	      eventLikelihood->Eval(combLikelihood, "A"); // add combi pdf
       }
 
       logEventLikelihood->Eval(null);
@@ -67,26 +65,33 @@ void IdeogramAnalyzer::Analyze(TString cuts, int i, int j) {
     	  logEventLikelihood->SetBinContent(i, -2*TMath::Log(eventLikelihood->GetBinContent(i)));
       }
 
-      logSumLikelihood->Add(logEventLikelihood);
+      sumLogLikelihood->Add(logEventLikelihood);
     }
 
   }
 
-  logSumLikelihood->SetAxisRange(logSumLikelihood->GetMinimum(0), logSumLikelihood->GetMaximum(), "Y");
-  logSumLikelihood->Draw();
+  sumLogLikelihood->SetAxisRange(sumLogLikelihood->GetMinimum(0), sumLogLikelihood->GetMaximum(), "Y");
+  sumLogLikelihood->Draw();
   
-  fitParabola->SetParameter(2, logSumLikelihood->GetMinimum(0));
+  fitParabola->SetParameter(2, sumLogLikelihood->GetMinimum(0));
   fitParabola->SetParameter(1, 1000);
   
-  fitParabola->SetRange(firstbin+(lastbin-firstbin)/bins*(logSumLikelihood->GetMinimumBin()-6), firstbin+(lastbin-firstbin)/bins*(logSumLikelihood->GetMinimumBin()+5));
+  fitParabola->SetRange(sumLogLikelihood->GetBinCenter(sumLogLikelihood->GetMinimumBin()) - 2, sumLogLikelihood->GetBinCenter(sumLogLikelihood->GetMinimumBin()) + 2);
 
-  logSumLikelihood->Fit("fitParabola","BWR");
+  sumLogLikelihood->Fit("fitParabola","BWR");
   
-  fMass      = fitParabola->GetParameter(0);
-  if (TMath::Sqrt(1/fitParabola->GetParameter(1)) < TMath::Sqrt(fMass)) {
-    fMassError = TMath::Sqrt(1/fitParabola->GetParameter(1));
+  if (firstbin+1 < fitParabola->GetParameter(0) && fitParabola->GetParameter(0) < lastbin-1) {
+    fMass = fitParabola->GetParameter(0);
+    if (TMath::Sqrt(1/fitParabola->GetParameter(1)) < TMath::Sqrt(fMass)) {
+      fMassError = TMath::Sqrt(1/fitParabola->GetParameter(1));
+    }
+    else fMassError = -1;
   }
-  fMassSigma = 0;
+  else {
+    fMass = -1;
+    fMassError = -1;
+  }
+  fMassSigma = -1;
   
   TString path("plot/Ideogram/"); path+= fIdentifier; path += "_"; path += i; path += "_"; path += j; path += ".png";
   ctemp->Print(path);
