7bf33586be651db74a9c4d16c6375353cf93a987
Markus Seidel <markus.seidel@desy.de>
2011-06-06 14:09:57 +0200

    Reworked comb likelihood

--- Analyzer/inc/IdeogramCombLikelihood.h
+++ Analyzer/inc/IdeogramCombLikelihood.h
@@ -2,12 +2,12 @@
 
 class IdeogramCombLikelihood {
   private:
-    double Signal(double *x, double *p);
+    double PCP(double *x, double *p);
+    double PWP(double *x, double *p);
+    double PMJ(double *x, double *p);
   
   public:
     IdeogramCombLikelihood() {};
     
     double Evaluate(double *x, double *p);
-    double CombBackground(double *x, double *p);
-    double CrystalBall(double *x, double *p);
 };
--- Analyzer/src/IdeogramCombLikelihood.cxx
+++ Analyzer/src/IdeogramCombLikelihood.cxx
@@ -1,12 +1,18 @@
 #include "IdeogramCombLikelihood.h"
 
 double IdeogramCombLikelihood::Evaluate(double *x, double *p) {
-  double fSig = 0.8;
+  
+  //* worst case, improvable by f(w_i)
+  double fCP = 0.40;
+  double fWP = 0.25;
+  double fMJ = 0.35;
+  //*/
 
-  return p[2] * (fSig * Signal(x, p) + (1.-fSig) * CrystalBall(x, p));
+  return p[2] * (fCP * PCP(x, p) + fWP * PWP(x, p) + fMJ * PMJ(x, p));
 }
 
-double IdeogramCombLikelihood::Signal(double *x, double *p) {
+
+double IdeogramCombLikelihood::PCP(double *x, double *p) {
   double xx = x[0];
   
   double p0 = 1.00528e+01 + 9.34552e-01 * xx;
@@ -14,38 +20,6 @@ double IdeogramCombLikelihood::Signal(double *x, double *p) {
   return TMath::Voigt(p[0] - p0, p[1], 2);
 }
 
-double IdeogramCombLikelihood::CombBackground(double *x, double *p) {
-  double xx = x[0];
-
-  /* Landau shape  
-  double p0 =  1;
-  double p1 =  1.90866e+01 + 8.61702e-01 * xx;
-  double p2 = -1.87389e+01 + 2.42629e-01 * xx;
-  
-  return TMath::Landau(p[0], p1, p2, 1);
-  //*/
-  // TMath::Landau([0], 3.45407e+01 + 7.03241e-01 * x, -2.33037e+01 + 2.41050e-01 * x)
-  
-  /* LogNormal shape
-  double p0 = 0.789244 - 0.00208923 * xx;
-  double p1 = 75.;
-  double p2 = 9.88908 + 0.589359 * xx;
-  
-  return TMath::LogNormal(p[0], p0, p1, p2);
-  //*/
-  
-  /* double gaus shape
-  double p0 =  4.10310e+00 - 2.01348e-02 * xx;
-  if (p0 < 0) p0 = 0;
-  double p1 =  5.85993e+01 + 6.02224e-01 * xx;
-  double p2 =  20.;
-  double p4 =  1.65129e+02 + 2.56381e-01 * xx;
-  if (p4 < 0) p4 = 0;
-  double p5 =  50.;
-  
-  return 1./(p0 + 1.) * (p0 * TMath::Gaus(p[0], p1, p2, 1) + TMath::Gaus(p[0], p4, p5, 1));
-  //*/
-}
 
 namespace cb {
   double A(double alpha, double power) {
@@ -56,15 +30,41 @@ namespace cb {
   };
 }
 
-// 7 parameters: [0] -> [6]
-double IdeogramCombLikelihood::CrystalBall(double* x, double* p)
+
+double IdeogramCombLikelihood::PWP(double* x, double* p)
 {
   double xx     = x[0];
   
   double N      =  1./0.01;
-  double mu     =  3.56187e+01 + 7.53376e-01 * xx;
-  double sigma  = -2.23761e+01 + 2.60009e-01 * xx;
-  double alpha  = -1.40038e-01 + 3.53449e-03 * xx;
+  double mu     =  8.25340e+00 + 9.02973e-01 * xx;
+  double sigma  = -2.77001e+01 + 2.97103e-01 * xx;
+  double alpha  = -5.46152e-01 + 5.40408e-03 * xx;
+  double power  =  15;
+  double t = (p[0] - mu) / sigma;
+  
+  //*
+  double N1 = -sqrt(TMath::PiOver2()) * sigma * (-1+TMath::Erf(-sigma*alpha)/(sqrt(2)*sigma));
+  double N2 = cb::A(alpha,power) * sigma/(-1.+power) * ( 0. + //lim(x->inf)
+                pow(((cb::B(alpha,power) * sigma - sigma*alpha)/sigma), (1. - power))
+              );
+  N = N1 + N2;
+  //*/
+
+  if(t < alpha)
+    return 1./N * TMath::Exp(-t*t/2);
+  else
+    return 1./N * cb::A(alpha,power) * TMath::Power(cb::B(alpha,power) + t, -power);
+}
+
+
+double IdeogramCombLikelihood::PMJ(double* x, double* p)
+{
+  double xx     = x[0];
+  
+  double N      =  1./0.01;
+  double mu     =  4.20677e+01 + 7.22396e-01 * xx;
+  double sigma  = -5.18172e+00 + 1.57942e-01 * xx;
+  double alpha  = -1.22146e-01 + 3.89904e-03 * xx;
   double power  =  15;
   double t = (p[0] - mu) / sigma;
   
