5bda1671c4a450d4a36c4e99ed816491579b9395
Markus Seidel <markus.seidel@desy.de>
2013-10-10 11:20:06 +0200

    Controlplots stacked, multiple backgrounds, signal split into permutation types, error reappeared :(

--- Analyzer/root/controlPlots.cc
+++ Analyzer/root/controlPlots.cc
@@ -12,10 +12,15 @@
 //#include "TSeqCollection.h"
 //#include "TGraphAsymmErrors.h"
 //#include "TPaveLabel.h"
+#include "THStack.h"
+#include "TPad.h"
+#include "TLegend.h"
 
 #include "ProgramOptionsReader.h"
+#include "Helper.h"
 
 #include <iostream>
+#include <boost/range/adaptor/reversed.hpp>
 
 typedef ProgramOptionsReader po;
 
@@ -28,6 +33,14 @@ void TopMassControlPlots::doPlots()
 {
   TH1::SetDefaultSumw2();
   gStyle->SetOptStat(0);
+  Helper* helper = new Helper();
+  helper->SetTDRStyle();
+  gStyle->SetPadLeftMargin(0.2);
+  gStyle->SetPadRightMargin(0.05);
+  gStyle->SetNdivisions(505, "X");
+	gStyle->SetTitleYOffset(1.75);
+  gStyle->SetOptStat(0);
+  gStyle->SetHatchesLineWidth(1.5);
 
   std::string path(po::GetOption<std::string>("analysisConfig.samplePath"));
 
@@ -35,7 +48,8 @@ void TopMassControlPlots::doPlots()
   TChain* myChain = new TChain("analyzeKinFit/eventTree");
   myChain->Add((path+std::string("Z2_S12_P11TeV_sig.root")).c_str());
 
-  hists.push_back(MyHistogram("1", "top.fitTop1[0].M()", myChain, ";m_{t}^{fit} [GeV]; Events", 125, 100, 350));
+  hists.push_back(MyHistogram("1", "top.fitTop1[0].M()", myChain, ";m_{t}^{fit} [GeV]; Events", 75, 50, 400));
+  /*
   hists.push_back(MyHistogram("2", "(top.recoW1[0].M()+top.recoW2[0].M())/2.0", myChain, ";m_{W}^{reco} [GeV]; Events", 60, 65, 125));
   hists.push_back(MyHistogram("3", "top.fitProb[0]", myChain, ";P_{gof}; Events", 50, 0, 1.0));
   hists.push_back(MyHistogram("4", "sqrt(pow(top.fitB1[0].Eta()-top.fitB2[0].Eta(),2) + pow(TVector2::Phi_mpi_pi(top.fitB1[0].Phi()-top.fitB2[0].Phi()),2))", myChain, ";#DeltaR_{b#bar{b}}; Events", 50, 1, 6));
@@ -46,29 +60,41 @@ void TopMassControlPlots::doPlots()
   hists.push_back(MyHistogram("9", "jet.jet[4].Pt()", myChain, ";p_{T}^{5} [GeV]; Events", 50, 0, 150));
   hists.push_back(MyHistogram("0", "jet.jet[5].Pt()", myChain, ";p_{T}^{6} [GeV]; Events", 50, 0, 100));
   hists.push_back(MyHistogram("A", "top.fitTop1[0].Pt()", myChain, ";p_{T,t}^{fit} [GeV]; Events", 100, 0, 1000));
+  */
+  
+  samples.push_back(MySample("Data", "MJP12*v1_data.root", kData, kBlack));
+  samples.push_back(MySample("t#bar{t}", "Z2_S12_ABS_JES_100_169_5_sig.root", kSig, kRed+1));
+  samples.push_back(MySample("QCD", "QCDMixing_MJPS12*v1_data.root", kBkg, kYellow));
 
   TTreeFormula weight("weight", po::GetOption<std::string>("weight").c_str(), myChain);
   TTreeFormula sel   ("sel"   , po::GetOption<std::string>("analysisConfig.selection").c_str(), myChain);
+  TTreeFormula selCP ("selCP" , (po::GetOption<std::string>("analysisConfig.selection")
+            +std::string(" && ")+po::GetOption<std::string>("analysisConfig.selectionCP")).c_str(), myChain);
+  TTreeFormula selWP ("selWP" , (po::GetOption<std::string>("analysisConfig.selection")
+            +std::string(" && ")+po::GetOption<std::string>("analysisConfig.selectionWP")).c_str(), myChain);
+  TTreeFormula selUN ("selUN" , (po::GetOption<std::string>("analysisConfig.selection")
+            +std::string(" && ")+po::GetOption<std::string>("analysisConfig.selectionUN")).c_str(), myChain);
 
   delete myChain;
 
-  for(MyHistogram& hist : hists){
-    hist.AddSignal();
-    hist.AddBackground();
-  }
   {
-    int counter = -1;
-    for(std::string file : {"MJP12*v1_data.root", "Z2_S12_ABS_JES_100_172_5_sig.root", "QCDMixing_MJPS12*v1_data.root"}){
-      ++counter;
-
+    int bkgCounter = -1;
+    for(MySample& sample : samples){
+      if(sample.type == kBkg ) ++bkgCounter;
       TChain* chain = new TChain("analyzeKinFit/eventTree");
-      int nFiles = chain->Add((path+file).c_str());
-      std::cout << "Adding " << nFiles << " files for " << file << std::endl;
+      int nFiles = chain->Add((path+sample.file).c_str());
+      std::cout << "Adding " << nFiles << " files for " << sample.name << " (" << sample.file << ")" << std::endl;
 
-      for(MyHistogram& hist : hists)
+      for(MyHistogram& hist : hists) {
         hist.var->SetTree(chain);
+        if     (sample.type == kSig ) hist.AddSignal(&sample);
+        else if(sample.type == kBkg ) hist.AddBackground(&sample);
+      }
       weight.SetTree(chain);
       sel   .SetTree(chain);
+      selCP .SetTree(chain);
+      selWP .SetTree(chain);
+      selUN .SetTree(chain);
 
       for(int i = 0; ; ++i){
         long entry = chain->LoadTree(i);
@@ -78,16 +104,24 @@ void TopMassControlPlots::doPlots()
             hist.var->UpdateFormulaLeaves();
           weight.UpdateFormulaLeaves();
           sel   .UpdateFormulaLeaves();
+          selCP .UpdateFormulaLeaves();
+          selWP .UpdateFormulaLeaves();
+          selUN .UpdateFormulaLeaves();
         }
         if(!weight.GetNdata()) continue;
         if(!sel   .GetNdata()) continue;
+
         for(int j = 0, l = sel.GetNdata(); j < l; ++j){
           if(!sel.EvalInstance(j)) continue;
           for(MyHistogram& hist : hists){
             if(!hist.var->GetNdata()) continue;
-            if     (counter == 0) hist.data  ->Fill(hist.var->EvalInstance(j));
-            else if(counter == 1) hist.sig[0]->Fill(hist.var->EvalInstance(j), weight.EvalInstance(j));
-            else if(counter == 2) hist.bkg[0]->Fill(hist.var->EvalInstance(j));
+            if     (sample.type == kData) hist.data           ->Fill(hist.var->EvalInstance(j));
+            else if(sample.type == kSig ) {
+              if(selCP.EvalInstance(j)) hist.sig[2]->Fill(hist.var->EvalInstance(j), weight.EvalInstance(j));
+              if(selWP.EvalInstance(j)) hist.sig[1]->Fill(hist.var->EvalInstance(j), weight.EvalInstance(j));
+              if(selUN.EvalInstance(j)) hist.sig[0]->Fill(hist.var->EvalInstance(j), weight.EvalInstance(j));
+            }
+            else if(sample.type == kBkg ) hist.bkg[bkgCounter]->Fill(hist.var->EvalInstance(j));
           }
         }
       }
@@ -96,22 +130,34 @@ void TopMassControlPlots::doPlots()
   for(MyHistogram& hist : hists){
     int bins = hist.data->GetNbinsX()+1;
     double integralD = hist.data->Integral(0,bins);
-    double integralS = hist.sig[0]->Integral(0,bins);
-    double integralB = hist.bkg[0]->Integral(0,bins);
+    double integralS = 0; for(TH1F* sig : hist.sig) integralS += sig->Integral(0,bins);
+    double integralB = 0; for(TH1F* bkg : hist.bkg) integralB += bkg->Integral(0,bins);
     double fSig = po::GetOption<double>("templates.fSig");
-    hist.sig[0]->Scale(    fSig *integralD/integralS);
-    hist.bkg[0]->Scale((1.-fSig)*integralD/integralB);
-    TCanvas* canv = new TCanvas();
+    for(TH1F* sig : hist.sig) sig->Scale(    fSig *integralD/integralS);
+    for(TH1F* bkg : hist.bkg) bkg->Scale((1.-fSig)*integralD/integralB);
+    TCanvas* canv = new TCanvas("cControlPlots", "cControlPlots", 600, 600);
     canv->cd();
-    hist.data  ->Draw("p");
-    hist.sig[0]->Draw("hist same");
-    hist.bkg[0]->Draw("hist same");
-    TH1F* comb = (TH1F*)hist.sig[0]->Clone();
-    comb->Add(hist.bkg[0]);
-    comb->SetLineColor(8);
-    comb->SetLineStyle(0);
-    comb->Draw("hist same");
-    canv->Print((std::string(hist.data->GetName())+std::string(".eps")).c_str(),"eps");
-    canv->Print((std::string(hist.data->GetName())+std::string(".png")).c_str(),"png");
+    
+    hist.data->GetYaxis()->SetRangeUser(1, hist.data->GetMaximum()*1.6);
+    hist.data->Draw("p");
+    THStack* stack = new THStack("stack", "");
+    for(TH1F* bkg : boost::adaptors::reverse(hist.bkg)) stack->Add(bkg);
+    for(TH1F* sig : boost::adaptors::reverse(hist.sig)) stack->Add(sig);
+    stack    ->Draw("hist same");
+    hist.data->Draw("p same");
+    
+    TLegend* leg0 = new TLegend(0.25, 0.75, 0.55, 0.925);
+    leg0->SetTextSize(0.03);
+    leg0->SetFillStyle(0);
+    leg0->SetBorderSize(0);
+    leg0->AddEntry( hist.data, hist.data->GetTitle(), "P" );
+    for(TH1F* sig : hist.sig) leg0->AddEntry( sig, sig->GetTitle(), "F" );
+    for(TH1F* bkg : hist.bkg) leg0->AddEntry( bkg, bkg->GetTitle(), "F" );
+    leg0->Draw();
+    
+    gPad->RedrawAxis();
+    
+    canv->Print((std::string("plot/controlplots/")+std::string(hist.data->GetName())+std::string(".eps")).c_str(),"eps");
+    canv->Print((std::string("plot/controlplots/")+std::string(hist.data->GetName())+std::string(".png")).c_str(),"png");
   }
 }
--- Analyzer/root/controlPlots.h
+++ Analyzer/root/controlPlots.h
@@ -7,6 +7,8 @@
 
 #include <vector>
 
+enum sampleType {kData, kSig, kBkg};
+
 class TopMassControlPlots{
 public:
   TopMassControlPlots();
@@ -14,31 +16,46 @@ public:
 
 private:
 
+  class MySample{
+  public:
+    MySample(std::string name_, std::string file_, int type_, int color_) :
+      name(name_), file(file_), type(type_), color(color_) {}
+    
+    std::string name, file;
+    int type, color;
+  };
+
   class MyHistogram{
   public:
     MyHistogram(std::string name, std::string formula, TChain* chain, std::string title, int nBins, double min, double max) :
       var(new TTreeFormula((std::string("f")+name).c_str(), formula.c_str(), chain)),
-      data(new TH1F((std::string("hD")+name).c_str(), title.c_str(), nBins, min, max))
+      data(new TH1F((std::string("hD")+name).c_str(), (std::string("Data")+title).c_str(), nBins, min, max))
     {
-      data->SetLineWidth(2);
+      data->SetLineWidth(1);
       data->SetLineColor(kBlack);
       data->SetMarkerStyle(20);
       data->SetMarkerColor(kBlack);
     }
 
-    void AddSignal()
+    void AddSignal(MySample* sample)
     {
-      sig.push_back((TH1F*)data->Clone());
-      sig.back()->SetLineWidth(2);
-      sig.back()->SetLineColor(kRed);
-      sig.back()->SetLineStyle(9);
+      int colorShift[] =  {-11, -8, 0};
+      std::string combinationType[] = {" unmatched", " wrong", " correct"};
+      for(int i = 0; i < 3; ++i) {
+        sig.push_back((TH1F*)data->Clone());
+        sig.back()->Reset();
+        sig.back()->SetLineWidth(1);
+        sig.back()->SetFillColor(sample->color+colorShift[i]);
+        sig.back()->SetTitle((sample->name + combinationType[i]).c_str());
+      }
     }
-    void AddBackground()
+    void AddBackground(MySample* sample)
     {
       bkg.push_back((TH1F*)data->Clone());
-      bkg.back()->SetLineWidth(2);
-      bkg.back()->SetLineColor(kBlue);
-      bkg.back()->SetLineStyle(2);
+      bkg.back()->Reset();
+      bkg.back()->SetLineWidth(1);
+      bkg.back()->SetFillColor(sample->color);
+      bkg.back()->SetTitle(sample->name.c_str());
     }
     TTreeFormula* var;
     TH1F *data;
@@ -48,6 +65,7 @@ private:
   void doPlots();
 
   std::vector<MyHistogram> hists;
+  std::vector<MySample>    samples;
 };
 
 #endif /* TOPMASSCONTROLPLOTS_H */
