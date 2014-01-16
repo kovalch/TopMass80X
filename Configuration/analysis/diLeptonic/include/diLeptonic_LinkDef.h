d93d0b46b69b6b29c652641351eaf6c5f98dc76d
hauk <johauk@desy-cms012.desy.de>
2014-01-16 17:07:08 +0100

    Revert LinkDef for the moment to keep consistency before updating cmake

--- Configuration/analysis/diLeptonic/include/diLeptonic_LinkDef.h
+++ Configuration/analysis/diLeptonic/include/diLeptonic_LinkDef.h
@@ -1,9 +1,9 @@
 #ifndef dileptonic_ROOT_TREE_VECTOR_LINKDEF_H 
 #define dileptonic_ROOT_TREE_VECTOR_LINKDEF_H 1
 
-#ifdef __CINT__
+#include "../../common/include/common_LinkDef.h"
 
-#pragma link off all classes;
+#ifdef __CINT__
 
 #pragma link C++ class TopAnalysis+;
 
