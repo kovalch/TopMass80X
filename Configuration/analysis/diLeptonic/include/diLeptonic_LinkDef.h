ea760678426410679d8278b69d3608e6688290e1
hauk <johauk@desy-cms012.desy.de>
2014-01-16 16:08:18 +0100

    Rename all LinkDef.h to unique name, rename project common to ntupleCommon

--- Configuration/analysis/diLeptonic/include/diLeptonic_LinkDef.h
+++ Configuration/analysis/diLeptonic/include/diLeptonic_LinkDef.h
@@ -1,9 +1,9 @@
 #ifndef dileptonic_ROOT_TREE_VECTOR_LINKDEF_H 
 #define dileptonic_ROOT_TREE_VECTOR_LINKDEF_H 1
 
-#include "../../common/include/LinkDef.h"
-
 #ifdef __CINT__
+
+#pragma link off all classes;
 
 #pragma link C++ class TopAnalysis+;
 
--- Configuration/analysis/ttH/include/ttH_LinkDef.h
+++ Configuration/analysis/ttH/include/ttH_LinkDef.h
@@ -1,9 +1,9 @@
 #ifndef tth_ROOT_TREE_VECTOR_LINKDEF_H 
 #define tth_ROOT_TREE_VECTOR_LINKDEF_H 1
 
-#include "../../common/include/LinkDef.h"
-
 #ifdef __CINT__
+
+#pragma link off all classes;
 
 #pragma link C++ class HiggsAnalysis+;
 
