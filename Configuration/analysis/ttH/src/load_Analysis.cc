ed9be2706c938ec4c6f8b83a17a806c1cff31eaa
hauk <johauk@desy-cms011.desy.de>
2014-02-20 12:14:17 +0100

    Added steering parameters for maxEvents and skipEvents

--- Configuration/analysis/ttH/src/load_Analysis.cc
+++ Configuration/analysis/ttH/src/load_Analysis.cc
@@ -104,10 +104,12 @@ void load_HiggsAnalysis(const TString& validFilenamePattern,
                         const Systematic::Systematic& systematic,
                         const int dy,
                         const int jetCategoriesId,
-                        const std::vector<AnalysisMode::AnalysisMode>& v_analysisMode)
+                        const std::vector<AnalysisMode::AnalysisMode>& v_analysisMode,
+                        const Long64_t& maxEvents,
+                        const Long64_t& skipEvents)
 {
     std::cout<<std::endl;
-
+    
     // Set up the channels to run over
     std::vector<Channel::Channel> channels;
     if(channel != Channel::undefined){
@@ -116,11 +118,11 @@ void load_HiggsAnalysis(const TString& validFilenamePattern,
     else{
         channels = Channel::realChannels;
     }
-
+    
     // Set up kinematic reconstruction
     KinematicReconstruction* kinematicReconstruction(0);
     //kinematicReconstruction = new KinematicReconstruction();
-
+    
     // Set up pileup reweighter
     std::cout<<"--- Beginning preparation of pileup reweighter\n";
     ztop::PUReweighter* puReweighter = new ztop::PUReweighter();
@@ -132,13 +134,13 @@ void load_HiggsAnalysis(const TString& validFilenamePattern,
     std::cout<<"Using PU input file:\n"<<pileupInput<<std::endl;
     puReweighter->setDataTruePUInput(pileupInput.Data());
     std::cout<<"=== Finishing preparation of pileup reweighter\n\n";
-
+    
     // Set up lepton efficiency scale factors
     LeptonScaleFactors::Systematic leptonSFSystematic(LeptonScaleFactors::nominal);
     if(systematic == Systematic::lept_up) leptonSFSystematic = LeptonScaleFactors::vary_up;
     else if(systematic == Systematic::lept_down) leptonSFSystematic = LeptonScaleFactors::vary_down;
     const LeptonScaleFactors leptonScaleFactors(ElectronSFInputFILE, MuonSFInputFILE, leptonSFSystematic);
-
+    
     // Set up trigger efficiency scale factors (do it for all channels)
     TriggerScaleFactors::Systematic triggerSFSystematic(TriggerScaleFactors::nominal);
     if(systematic == Systematic::trig_up) triggerSFSystematic = TriggerScaleFactors::vary_up;
@@ -169,7 +171,7 @@ void load_HiggsAnalysis(const TString& validFilenamePattern,
         jetEnergyScaleScaleFactors = new JetEnergyScaleScaleFactors(JesUncertaintySourceFILE, jesSystematic);
     }
     
-
+    
     // Set up jet categories
     JetCategories* jetCategories(0);
     if(jetCategoriesId == 0) jetCategories = new JetCategories(2, 4, 1, 3, true, true); // Default categories
@@ -180,41 +182,41 @@ void load_HiggsAnalysis(const TString& validFilenamePattern,
         std::cerr<<"Error in load_Analysis! No jet categories defined\n...break\n"<<std::endl;
         exit(832);
     }
-
+    
     // Vector for setting up all analysers
     std::vector<AnalysisHistogramsBase*> v_analysisHistograms;
-
+    
     // Set up event yield histograms
     EventYieldHistograms* eventYieldHistograms(0);
     eventYieldHistograms = new EventYieldHistograms({"1", "2", "3", "4", "5", "6", "7"}, {"7"}, jetCategories);
     v_analysisHistograms.push_back(eventYieldHistograms);
-
+    
     // Set up Drell-Yan scaling histograms
     DyScalingHistograms* dyScalingHistograms(0);
     dyScalingHistograms = new DyScalingHistograms({"4", "5", "6", "7"}, "5");
     v_analysisHistograms.push_back(dyScalingHistograms);
-
+    
     // Set up basic histograms
     BasicHistograms* basicHistograms(0);
     if(std::find(v_analysisMode.begin(), v_analysisMode.end(), AnalysisMode::cp) != v_analysisMode.end()){
         basicHistograms = new BasicHistograms({"1", "2", "3", "4", "5", "6", "7"}, {"7"}, jetCategories);
         v_analysisHistograms.push_back(basicHistograms);
     }
-
+    
     // Set up jet charge analyzer
     JetChargeAnalyzer* jetChargeAnalyzer(0);
     if(std::find(v_analysisMode.begin(), v_analysisMode.end(), AnalysisMode::charge) != v_analysisMode.end()){
         jetChargeAnalyzer = new JetChargeAnalyzer({"7"});
         v_analysisHistograms.push_back(jetChargeAnalyzer);
     }
-
+    
     // Set up jet match analyzer
     JetMatchAnalyzer* jetMatchAnalyzer(0);
     if(std::find(v_analysisMode.begin(), v_analysisMode.end(), AnalysisMode::match) != v_analysisMode.end()){
         jetMatchAnalyzer = new JetMatchAnalyzer({"7"}, {"7"}, jetCategories);
         v_analysisHistograms.push_back(jetMatchAnalyzer);
     }
-
+    
     // Set up playground
     Playground* playground(0);
     if(std::find(v_analysisMode.begin(), v_analysisMode.end(), AnalysisMode::playg) != v_analysisMode.end()){
@@ -222,27 +224,27 @@ void load_HiggsAnalysis(const TString& validFilenamePattern,
         playground = new Playground({"1", "2", "3", "4", "5", "6", "7"});
         v_analysisHistograms.push_back(playground);
     }
-
+    
     // Set up DijetAnalyzer
     DijetAnalyzer* dijetAnalyzer(0);
     if(std::find(v_analysisMode.begin(), v_analysisMode.end(), AnalysisMode::dijet) != v_analysisMode.end()){
         dijetAnalyzer = new DijetAnalyzer(Mva2dWeightsFILE, "", "", {}, {"7"}, jetCategories);
         v_analysisHistograms.push_back(dijetAnalyzer);
     }
-
+    
     // Set up MVA validation, including reading in MVA weights in case they exist
     MvaValidation* mvaValidation(0);
     if(std::find(v_analysisMode.begin(), v_analysisMode.end(), AnalysisMode::mvaA) != v_analysisMode.end()){
         mvaValidation = new MvaValidation(Mva2dWeightsFILE, {"7"}, {"7"}, jetCategories);
         v_analysisHistograms.push_back(mvaValidation);
     }
-
+    
     // Set up production of MVA input tree
     MvaTreeHandler* mvaTreeHandler(0);
     if(std::find(v_analysisMode.begin(), v_analysisMode.end(), AnalysisMode::mvaP) != v_analysisMode.end()){
         mvaTreeHandler = new MvaTreeHandler(MvaInputDIR, {"7"}, {"7"}, jetCategories);
     }
-
+    
     // Set up the analysis
     HiggsAnalysis* selector = new HiggsAnalysis();
     selector->SetAnalysisOutputBase(AnalysisOutputDIR);
@@ -256,18 +258,18 @@ void load_HiggsAnalysis(const TString& validFilenamePattern,
     
     selector->SetMvaInputProduction(mvaTreeHandler);
     selector->SetAllAnalysisHistograms(v_analysisHistograms);
-
+    
     // Access selectionList containing all input sample nTuples
     ifstream infile("selectionList.txt");
     if(!infile.good()){
         std::cerr<<"Error! Please check the selectionList.txt file!\n"<<std::endl;
         exit(773);
     }
-
+    
     // Loop over all input files
     int filecounter = 0;
     while(!infile.eof()){
-
+        
         // Access nTuple input filename from selectionList
         TString filename;
         infile>>filename;
@@ -276,21 +278,21 @@ void load_HiggsAnalysis(const TString& validFilenamePattern,
         std::cout<<std::endl;
         std::cout<<"PROCESSING File "<<++filecounter<<" ("<<filename<<") from selectionList.txt"<<std::endl;
         std::cout<<std::endl;
-
+        
         // Open nTuple file
         TFile file(filename);
         if(file.IsZombie()){
             std::cerr<<"ERROR! Cannot open nTuple file with name: "<<filename<<std::endl;
             exit(853);
         }
-
+        
         // Check whether nTuple can be found
         TTree* tree = dynamic_cast<TTree*>(file.Get("writeNTuple/NTuple"));
         if(!tree){
             std::cerr<<"ERROR! TTree (=nTuple) not found in file!\n"<<std::endl;
             exit(854);
         }
-
+        
         // Access information about samples, stored in the nTuples
         TObjString* channel_from_file = dynamic_cast<TObjString*>(file.Get("writeNTuple/channelName"));
         TObjString* systematics_from_file = dynamic_cast<TObjString*>(file.Get("writeNTuple/systematicsName"));
@@ -303,29 +305,29 @@ void load_HiggsAnalysis(const TString& validFilenamePattern,
             std::cout<<"Error: info about sample missing!"<<std::endl;
             return;
         }
-
+        
         // Configure information about samples
         const bool isTopSignal = o_isSignal->GetString() == "1";
         const bool isMC = o_isMC->GetString() == "1";
         const bool isHiggsSignal(o_isHiggsSignal && o_isHiggsSignal->GetString()=="1");
         const bool isHiggsInclusive(isHiggsSignal && samplename->GetString()=="ttbarhiggsinclusive");
         const bool isTtbarV(samplename->GetString()=="ttbarw" || samplename->GetString()=="ttbarz");
-
+        
         if(!isMC && systematic!=Systematic::undefined){
             std::cout<<"Sample is DATA, so not running again for systematic variation\n";
             continue;
         }
-
+        
         // Is the channel given in the file?
         // FIXME: In case of data, use the channel as specified in the file, or what is this for?
         if(channel_from_file->GetString() != ""){
             channels.clear();
             channels.push_back(Channel::convertChannel(static_cast<std::string>(channel_from_file->GetString())));
         }
-
+        
         // Loop over channels and run selector
         for(const auto& selectedChannel : channels){
-
+            
             // Set output file name
             TString outputfilename(filename);
             if(outputfilename.Contains('/')){
@@ -346,7 +348,7 @@ void load_HiggsAnalysis(const TString& validFilenamePattern,
             }
             if(isHiggsInclusive)outputfilename.ReplaceAll("inclusive", "inclusiveOther");
             if(isTopSignal)outputfilename.ReplaceAll("signalplustau", "signalPlusOther");
-
+            
             // Configure selector
             selector->SetChannel(channelName);
             selector->SetTopSignal(isTopSignal);
@@ -371,27 +373,27 @@ void load_HiggsAnalysis(const TString& validFilenamePattern,
             selector->SetRunWithTtbb(0);
             selector->SetHiggsInclusiveSample(isHiggsInclusive);
             selector->SetHiggsInclusiveSeparation(false);
-
+            
             // Set up nTuple chain and run selector
             TChain chain("writeNTuple/NTuple");
             chain.Add(filename);
             // chain.SetProof(); //will work from 5.34 onwards
-            chain.Process(selector);
-
+            chain.Process(selector, "", maxEvents, skipEvents);
+            
             // For splitting of dileptonic ttbar in tt+bb and tt+other
             if(isTopSignal && !isHiggsSignal && !isTtbarV){
                 selector->SetRunWithTtbb(1);
                 outputfilename.ReplaceAll("Other", "Bbbar");
                 selector->SetOutputfilename(outputfilename);
-                chain.Process(selector);
+                chain.Process(selector, "", maxEvents, skipEvents);
             }
-
+            
             // For splitting of ttH inclusive decay in H->bb and other decays
             if(isHiggsInclusive){
                 selector->SetHiggsInclusiveSeparation(true);
                 outputfilename.ReplaceAll("Other", "Bbbar");
                 selector->SetOutputfilename(outputfilename);
-                chain.Process(selector);
+                chain.Process(selector, "", maxEvents, skipEvents);
             }
         }
         file.Close();
@@ -407,40 +409,51 @@ int main(int argc, char** argv) {
     CLParameter<std::string> opt_systematic("s", "Run with a systematic that runs on the nominal ntuples, e.g. 'PU_UP'", false, 1, 1,
             common::makeStringCheck(Systematic::convertSystematics(Systematic::allowedSystematicsHiggsAnalysis)));
     CLParameter<int> opt_dy("d", "Drell-Yan mode (11 for ee, 13 for mumu, 15 for tautau)", false, 1, 1,
-            [](int dy){return dy == 11 || dy == 13 || dy == 15;});
+            [](int dy){return dy==11 || dy==13 || dy==15;});
     CLParameter<int> opt_jetCategoriesId("j", "ID for jet categories (# jets, # b-jets). If not specified, use default categories (=0)", false, 1, 1,
             [](int id){return id>=0 && id<=3;});
     CLParameter<std::string> opt_mode("m", "Mode of analysis: control plots (cp), Produce MVA input (mvaP), Apply MVA weights (mvaA), dijet analyser (dijet), playground (playg), charge analyser (charge), jet match analyser (match). Default is cp", false, 1, 100,
             common::makeStringCheck(AnalysisMode::convertAnalysisModes(AnalysisMode::allowedAnalysisModes)));
+    CLParameter<Long64_t> opt_maxEvents("maxEvents", "Maximum number of events to process", false, 1, 1,
+            [](const Long64_t mE){return mE > 0;});
+    CLParameter<Long64_t> opt_skipEvents("skipEvents", "Number of events to be skipped", false, 1, 1,
+            [](const Long64_t sE){return sE > 0;});
     CLAnalyser::interpretGlobal(argc, argv);
-
+    
     // Set up a pattern for selecting only files from selectionList containing that pattern in filename
     const TString validFilenamePattern = opt_filenamePattern.isSet() ? opt_filenamePattern[0] : "";
-
+    
     // Set up channel
     Channel::Channel channel(Channel::undefined);
     if(opt_channel.isSet()) channel = Channel::convertChannel(opt_channel[0]);
-
+    
     // Set up systematic
     Systematic::Systematic systematic(Systematic::undefined);
     if(opt_systematic.isSet()) systematic = Systematic::convertSystematic(opt_systematic[0]);
-
+    
     // Set up Drell-Yan mode
     const int dy = opt_dy.isSet() ? opt_dy[0] : 0;
-
+    
     // Set up jet categories
     const int jetCategoriesId = opt_jetCategoriesId.isSet() ? opt_jetCategoriesId[0] : 0;
-
+    
     // Set up analysis mode
     std::vector<AnalysisMode::AnalysisMode> v_analysisMode({AnalysisMode::cp});
     if(opt_mode.isSet()) v_analysisMode = AnalysisMode::convertAnalysisModes(opt_mode.getArguments());
     std::cout<<"\nRunning the following analysis modes:\n";
     for(const auto& analysisMode : v_analysisMode) std::cout<<AnalysisMode::convertAnalysisMode(analysisMode)<<" , ";
     std::cout<<"\n\n";
-
+    
+    // Set up maximum number of events to process
+    const Long64_t bigNumber(TChain::kBigNumber);
+    const Long64_t maxEvents = opt_maxEvents.isSet() ? opt_maxEvents[0] : bigNumber;
+    
+    // Set up number of events to be skipped
+    const Long64_t skipEvents = opt_skipEvents.isSet() ? opt_skipEvents[0] : 0;
+    
     // Start plotting
     //TProof* p = TProof::Open(""); // not before ROOT 5.34
-    load_HiggsAnalysis(validFilenamePattern, channel, systematic, dy, jetCategoriesId, v_analysisMode);
+    load_HiggsAnalysis(validFilenamePattern, channel, systematic, dy, jetCategoriesId, v_analysisMode, maxEvents, skipEvents);
     //delete p;
 }
 
