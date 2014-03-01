1fd3837aa7c0b44300334bd7047ccda006990f2d
Markus Seidel <markus.seidel@desy.de>
2014-03-01 08:51:31 +0100

    Systematic uncertainties with sign and for 1D

--- Analyzer/root/ensembleTreeSysLeptonJets.C
+++ Analyzer/root/ensembleTreeSysLeptonJets.C
@@ -100,11 +100,11 @@ void ensembleTreeSysLeptonJets(TString sPath = globalPath)
   ensembles["fSig1.0"] = ensemble("fsig_1.00/job_*_ensemble.root", 7000000./1.75);
   ensembles["fSig0.9"] = ensemble("fsig_0.90/job_*_ensemble.root", 7000000./1.75);
   
-  ensembles["matchingUp"] = ensemble("Summer12_TTJets1725_matchingup/job_*_ensemble.root", 37083003./1.75);
-  ensembles["matchingDown"] = ensemble("Summer12_TTJets1725_matchingdown/job_*_ensemble.root", 34053113./1.75);
+  ensembles["matchingUp"] = ensemble("Summer12_TTJets1725_matchingup/job_*_ensemble.root", 5415010./1.75);
+  ensembles["matchingDown"] = ensemble("Summer12_TTJets1725_matchingdown/job_*_ensemble.root", 5476728./1.75);
   
-  ensembles["scaleUp"] = ensemble("Summer12_TTJets1725_scaleup/job_*_ensemble.root", 41908271./1.75);
-  ensembles["scaleDown"] = ensemble("Summer12_TTJets1725_scaledown/job_*_ensemble.root", 39286663./1.75);
+  ensembles["scaleUp"] = ensemble("Summer12_TTJets1725_scaleup/job_*_ensemble.root", 5009488./1.75);
+  ensembles["scaleDown"] = ensemble("Summer12_TTJets1725_scaledown/job_*_ensemble.root", 5387181./1.75);
   
   ensembles["jerUp"] = ensemble("Summer12_TTJets1725_jer:up/job_*_ensemble.root", 7000000./1.75);
   ensembles["jerDown"] = ensemble("Summer12_TTJets1725_jer:down/job_*_ensemble.root", 7000000./1.75);
@@ -125,33 +125,44 @@ void ensembleTreeSysLeptonJets(TString sPath = globalPath)
   ensembles["P11mpiHi"] = ensemble("Summer12_TTJets1725_MGDecays_P11mpiHi/job_*_ensemble.root", 18000000./1.75);
   ensembles["P11TeV"] = ensemble("Summer12_TTJets1725_MGDecays_P11TeV/job_*_ensemble.root", 18000000./1.75);
   
+  ensembles["eesUp"] = ensemble("Summer12_TTJets1725_eesShift_+1/job_*_ensemble.root", 7000000./1.75);
+  ensembles["eesDown"] = ensemble("Summer12_TTJets1725_eesShift_-1/job_*_ensemble.root", 7000000./1.75);
+  
+  ensembles["mesUp"] = ensemble("Summer12_TTJets1725_mesShift_+1/job_*_ensemble.root", 7000000./1.75);
+  ensembles["mesDown"] = ensemble("Summer12_TTJets1725_mesShift_-1/job_*_ensemble.root", 7000000./1.75);
+  
+  ensembles["metcl2"] = ensemble("Summer12_TTJets1725_metcl_2/job_*_ensemble.root", 7000000./1.75);
+  
   ///////////////////////////////////
   
-  comparisons["Pile-up"] = comparison("defaultNewWeights", "puUp", "puDown");
-  comparisons["JES FlavorPureQuark"] = comparison("defaultNewWeights", "flavorQUp", "flavorQDown", true, false);//
-  comparisons["JES FlavorPureGluon"] = comparison("defaultNewWeights", "flavorGUp", "flavorGDown", true, false);//
-  comparisons["JES FlavorPureBottom"] = comparison("default", "flavorBUp", "flavorBDown", true, false);
-  comparisons["JES FlavorQCD"] = comparison("default", "flavorQCDUp", "flavorQCDDown");
-  comparisons["b-fragmentation"] = comparison("defaultNewWeights", "bFragLEP");
-  comparisons["Neutrino fraction"] = comparison("defaultNewWeights", "bFNuUp", "bFNuDown");
-  comparisons["b-tag"] = comparison("defaultNewWeights", "bTagSFUp", "bTagSFDown");//
-  comparisons["Mistag"] = comparison("defaultNewWeights", "misTagSFUp", "misTagSFDown");//
-  comparisons["Top-pt"] = comparison("defaultNewWeights", "topPt", "");//
-  comparisons["Matching"] = comparison("calibration", "matchingUp", "matchingDown", false);
-  comparisons["Scale"] = comparison("calibration", "scaleUp", "scaleDown", false);
-  comparisons["JER"] = comparison("default", "jerUp", "jerDown");
-  comparisons["JES total"] = comparison("default", "jesUp", "jesDown");
-  comparisons["JES PileUpPt"] = comparison("defaultNewWeights", "jesPuUp", "jesPuDown");
-  comparisons["MG vs. Powheg"] = comparison("calibration", "powheg", "", false, false);
-  comparisons["Powheg+Pythia vs. MCatNLO"] = comparison("powheg", "mcatnlo", "", false, false);
-  comparisons["Pythia vs. Herwig"] = comparison("powheg", "powhegHerwig", "", false, false);
-  comparisons["Powheg+Herwig vs. MCatNLO"] = comparison("powhegHerwig", "mcatnlo", "", false, false);
-  comparisons["MG (SC) vs. Powheg"] = comparison("powheg", "defaultSC", "", false);
-  comparisons["Spin correlations"] = comparison("calibration", "defaultSC", "", false, true);
-  comparisons["Pythia Z2* vs. P11"] = comparison("defaultSC", "P11", "", false, false);
-  comparisons["Color reconnection"] = comparison("P11", "P11noCR", "", false);
-  comparisons["Underlying event"] = comparison("P11", "P11mpiHi", "P11TeV", false);
-  comparisons["Background"] = comparison("defaultNewWeights", "fSig1.0", "fSig0.9");
+  comparisons["Pile-up (pp cross-section)       "] = comparison("defaultNewWeights", "puUp", "puDown");
+  comparisons["Jet energy response (udsc)       "] = comparison("defaultNewWeights", "flavorQUp", "flavorQDown", true);//
+  comparisons["Jet energy response (gluon)      "] = comparison("defaultNewWeights", "flavorGUp", "flavorGDown", true);//
+  comparisons["Jet energy response (b)          "] = comparison("default", "flavorBUp", "flavorBDown", true);
+  comparisons["Jet energy response (FlavorQCD)  "] = comparison("default", "flavorQCDUp", "flavorQCDDown", true, false);
+  comparisons["b fragmentation                  "] = comparison("defaultNewWeights", "bFragLEP");
+  comparisons["Semi-leptonic B hadron decays    "] = comparison("defaultNewWeights", "bFNuUp", "bFNuDown");
+  comparisons["b-tag rate                       "] = comparison("defaultNewWeights", "bTagSFUp", "bTagSFDown");//
+  comparisons["b-tag (mistag rate)              "] = comparison("defaultNewWeights", "misTagSFUp", "misTagSFDown");//
+  comparisons["Top-pt reweighting               "] = comparison("defaultNewWeights", "topPt", "");//
+  comparisons["ME-PS matching threshold         "] = comparison("calibration", "matchingUp", "matchingDown", false);
+  comparisons["$Q^{2}$ scale                    "] = comparison("calibration", "scaleUp", "scaleDown", false);
+  comparisons["Jet energy resolution            "] = comparison("default", "jerUp", "jerDown");
+  comparisons["\\pt- and $\\eta$-dependent JES    "] = comparison("default", "jesUp", "jesDown");
+  comparisons["Pile-up (JES)                    "] = comparison("defaultNewWeights", "jesPuUp", "jesPuDown");
+  comparisons["MadGraph (no SC) vs. Powheg      "] = comparison("calibration", "powheg", "", false, false);
+  comparisons["Powheg+Pythia6 vs. MC@NLO+Herwig6"] = comparison("powheg", "mcatnlo", "", false, false);
+  comparisons["Powheg+Pythia6 vs. Powheg+Herwig6"] = comparison("powheg", "powhegHerwig", "", false, false);
+  comparisons["MC@NLO+Herwig6 vs. Powheg+Herwig6"] = comparison("mcatnlo", "powhegHerwig", "", false, false);
+  comparisons["ME generator                     "] = comparison("defaultSC", "powheg", "", false);
+  comparisons["Spin correlations                "] = comparison("calibration", "defaultSC", "", false, true);
+  comparisons["Pythia Z2* vs. P11               "] = comparison("defaultSC", "P11", "", false, false);
+  comparisons["Color reconnection               "] = comparison("P11", "P11noCR", "", false);
+  comparisons["Underlying event                 "] = comparison("P11", "P11mpiHi", "P11TeV", false);
+  comparisons["Non-\\ttbar background            "] = comparison("defaultNewWeights", "fSig0.9", "fSig1.0");
+  comparisons["Lepton energy scale (electron)   "] = comparison("defaultNewWeights", "eesUp", "eesDown");
+  comparisons["Lepton energy scale (muon)       "] = comparison("defaultNewWeights", "mesUp", "mesDown");
+  comparisons["MET                              "] = comparison("defaultNewWeights", "metcl2");
   
   
   ///////////////////////////////////
@@ -159,6 +170,8 @@ void ensembleTreeSysLeptonJets(TString sPath = globalPath)
   std::cout << "\n### Fitting pseudo-experiments" << std::endl;
   for(std::map<std::string, ensemble>::iterator it = ensembles.begin(); it != ensembles.end(); it++) {
     std::cout << std::setiosflags(std::ios::left) << std::setw(20) << it->first;
+    
+    int N_PE = 0;
     if (strcmp(it->second.file, "") == 0) {
       // Calibration uncertainties
       it->second.massUnc = 0.02;
@@ -183,22 +196,30 @@ void ensembleTreeSysLeptonJets(TString sPath = globalPath)
       it->second.chain->Fit("gaus", "mass_mTop", "mass_mTop>0 & genMass==172.5 & genJES==1", "LEMQ0");
       it->second.mass1d    = gaus->GetParameter(1);
       it->second.mass1dUnc = gaus->GetParameter(2) / sqrt(it->second.size/(crossSection*peLumi));
+      
+      N_PE = it->second.chain->GetEntries("mass_mTop_JES>0 & JES_mTop_JES>0 & genMass==172.5 & genJES==1");
     }
     
-    printf("\tmass = %.2lf+/-%.2lf GeV, jes = %.3lf+/-%.3lf, mass1d = %.2lf+/-%.2lf GeV\n", it->second.mass, it->second.massUnc, it->second.jes, it->second.jesUnc, it->second.mass1d, it->second.mass1dUnc);
+    printf("\tmass = %.2lf+/-%.2lf GeV, jes = %.3lf+/-%.3lf, mass1d = %.2lf+/-%.2lf GeV, N(PE) = %i\n", it->second.mass, it->second.massUnc, it->second.jes, it->second.jesUnc, it->second.mass1d, it->second.mass1dUnc, N_PE);
   }
   
   ///////////////////////////////////
   
   std::cout << "\n### Systematic uncertainties" << std::endl;
+  std::cout << "Uncertainty name & 2D mass & JSF & 1D mass \\tabularnewline\n\\hline" << std::endl;
   for(std::map<std::string, comparison>::iterator it = comparisons.begin(); it != comparisons.end(); it++) {
-    if (!it->second.active) std::cout << "#";
-    std::cout << std::setiosflags(std::ios::left) << std::setw(20) << it->first;
+    std::cout << "\\hline" <<std::endl;
+    if (!it->second.active) std::cout << "(cc) ";
+    std::cout << it->first;
     
     ensemble nominal  = ensembles.find(it->second.nominal)->second;
     ensemble up       = ensembles.find(it->second.up)->second;
     ensemble down;
-    if (strcmp(it->second.down, "") == 0) down = up;
+    bool upDown = true;
+    if (strcmp(it->second.down, "") == 0) {
+      down   = up;
+      upDown = false;
+    }
     else down         = ensembles.find(it->second.down)->second;
     
     double massShift    = max(abs(nominal.mass-up.mass), abs(nominal.mass-down.mass));
@@ -215,17 +236,28 @@ void ensembleTreeSysLeptonJets(TString sPath = globalPath)
       mass1dShiftUnc  = max(sqrt(pow(nominal.mass1dUnc,2)+pow(up.mass1dUnc,2)),sqrt(pow(nominal.mass1dUnc,2)+pow(down.mass1dUnc,2)));
     }
     
-    printf("\tmassShift = %.2lf+/-%.2lf GeV, jesShift = %.3lf+/-%.3lf, mass1dShift = %.2lf+/-%.2lf GeV\n", massShift, massShiftUnc, jesShift, jesShiftUnc, mass1dShift, mass1dShiftUnc);
+    printf(" & %.2lf$\\pm$%.2lf & %.3lf$\\pm$%.3lf & %.2lf$\\pm$%.2lf \\tabularnewline\n", massShift, massShiftUnc, jesShift, jesShiftUnc, mass1dShift, mass1dShiftUnc);
+    
+    if (upDown) {
+      if (!it->second.active) std::cout << "(cc) ";
+      printf("- up                              & %+.2lf & %+.3lf & %+.2lf \\tabularnewline\n", up.mass-nominal.mass, up.jes-nominal.jes, up.mass1d-nominal.mass1d);
+      if (!it->second.active) std::cout << "(cc) ";
+      printf("- down                            & %+.2lf & %+.3lf & %+.2lf \\tabularnewline\n", down.mass-nominal.mass, down.jes-nominal.jes, down.mass1d-nominal.mass1d);
+    }
+    else {
+      if (!it->second.active) std::cout << "(cc) ";
+      printf("- shift                           & %+.2lf & %+.3lf & %+.2lf \\tabularnewline\n", up.mass-nominal.mass, up.jes-nominal.jes, up.mass1d-nominal.mass1d);
+    }
     
     if (it->second.active) {
-      totalMassUncertainty2   += pow(massShift, 2);
-      totalJESUncertainty2    += pow(jesShift, 2);
-      totalMass1dUncertainty2 += pow(mass1dShift, 2);
+      totalMassUncertainty2   += pow(max(massShift,   massShiftUnc  ), 2);
+      totalJESUncertainty2    += pow(max(jesShift,    jesShiftUnc   ), 2);
+      totalMass1dUncertainty2 += pow(max(mass1dShift, mass1dShiftUnc), 2);
     }
   }
   
-  std::cout << "\n### Total" << std::endl;
-  printf("\tmassShift = %.2lf GeV, jesShift = %.3lf, mass1dShift = %.2lf GeV\n", sqrt(totalMassUncertainty2), sqrt(totalJESUncertainty2), sqrt(totalMass1dUncertainty2));
+  std::cout << "\n### Total uncertainties" << std::endl;
+  printf("\t 2D mass = %.2lf GeV, JSF = %.3lf, 1D mass = %.2lf GeV\n", sqrt(totalMassUncertainty2), sqrt(totalJESUncertainty2), sqrt(totalMass1dUncertainty2));
 }
 
 
