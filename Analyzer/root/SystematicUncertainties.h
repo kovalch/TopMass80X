2bbc53671ee977edc4e9fb56acedd92ac84bd444
Eike Schlieckau <eike.schlieckau@desy.de>
2014-03-04 18:21:53 +0100

    added program to calculate systematic uncertainties (starting from Markus macro) - right now only does lepton+jets channel - gives screen output (readable) and text file output (LaTeX format) - still few hardcoded parts, to be removed soon

---
+++ Analyzer/root/SystematicUncertainties.h
@@ -0,0 +1,60 @@
+#ifndef SYSTEMATICUNCERTAINTIES_H
+#define SYSTEMATICUNCERTAINTIES_H
+
+#include <map>
+#include <string>
+#include <vector>
+
+#include "TChain.h"
+
+class SystematicUncertainties {
+public:
+  SystematicUncertainties();
+  ~SystematicUncertainties() {}
+
+private:
+
+  struct ensemble {
+    std::string file; // file selection for chain
+    double size; // EFFECTIVE sample size
+    std::map<std::string, std::pair<double, double>> values;
+    TChain* chain;
+    ensemble(std::string f = "temp", double s = 0, std::vector<std::pair<std::string, std::pair<double,double>>> v = std::vector<std::pair<std::string, std::pair<double,double>>>(0))
+    : file(f), size(s), chain(0)
+    {
+      for(auto& var : v){
+        values[var.first] = var.second;
+      }
+    }
+  };
+
+  struct comparison {
+    std::string nominal;
+    std::string up;
+    std::string down;
+    bool correlated;
+    bool active;
+    comparison(std::string n = "", std::string u = "", std::string d = "", bool c = true, bool a = true)
+    : nominal(n), up(u), down(d), correlated(c), active(a) {}
+  };
+
+  struct dataSample {
+    std::map<std::string, ensemble> ensembles;
+    std::map<std::string, comparison> comparisons;
+    std::vector<std::string> variables;
+    std::string path;
+    double peLumi;
+    double crossSection;
+  };
+
+  void deriveSystematics();
+  void fillLeptonJets();
+
+  std::string getSelectionFromVariable(std::string& var);
+
+  dataSample sample;
+
+};
+
+#endif /* SYSTEMATICUNCERTAINTIES_H */
+
