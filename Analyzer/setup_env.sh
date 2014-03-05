c5f5041cdcd701b54939bc20e956fddab7cc6c0e
Eike Schlieckau <eike.schlieckau@desy.de>
2014-03-05 13:23:58 +0100

    - updated configs - corrected path in setup-env.sh - updated lumi in plot labels

--- Analyzer/setup_env.sh
+++ Analyzer/setup_env.sh
@@ -1,3 +1,3 @@
-export LD_LIBRARY_PATH=/afs/naf.desy.de/user/e/eschliec/wd/LHAPDF-605/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/afs/desy.de/user/e/eschliec/xxl-af-cms/wd/LHAPDF-605/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=/cvmfs/cms.cern.ch/slc5_amd64_gcc462/external/boost/1.51.0/lib:$LD_LIBRARY_PATH
 
