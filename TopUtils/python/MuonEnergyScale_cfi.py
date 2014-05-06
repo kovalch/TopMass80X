04544e3db5ed70fbd0f001b9134aaa4d0eab7e5f
Markus <markus.seidel@desy.de>
2014-05-06 09:48:26 +0200

    New muon energy uncertainty

--- TopUtils/python/MuonEnergyScale_cfi.py
+++ TopUtils/python/MuonEnergyScale_cfi.py
@@ -3,6 +3,6 @@ import FWCore.ParameterSet.Config as cms
 scaledMuonEnergy = cms.EDProducer("MuonEnergyScale",
       src         = cms.InputTag("patMuons"),
       mets        = cms.InputTag("patMETs"),
-      uncertainty = cms.double(0.01),
+      uncertainty = cms.double(0.002),
       shiftBy     = cms.double(0.)
 )
