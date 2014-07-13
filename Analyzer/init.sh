130f9d5f24d66704b725926e66124f4d877836cf
Markus Seidel <markus.seidel@desy.de>
2014-07-13 14:00:31 +0200

    Init script

---
+++ Analyzer/init.sh
@@ -0,0 +1,10 @@
+module use -a /afs/desy.de/group/cms/modulefiles/
+module load cmssw
+cmsenv
+PATH=/afs/desy.de/products/root/amd64_rhel50/5.34.00/bin:$PATH
+MANPATH=/afs/desy.de/products/root/amd64_rhel50/5.34.00/man:$MANPATH
+PYTHONPATH=/afs/desy.de/products/root/amd64_rhel50/5.34.00/lib:$PYTHONPATH
+LD_LIBRARY_PATH=/opt/d-cache/dcap/lib64:/afs/desy.de/products/root/amd64_rhel50/5.34.00/lib:/usr/lib64/perl5/5.10.0/x86_64-linux-thread-multi/CORE:$LD_LIBRARY_PATH
+ROOTSYS=/afs/desy.de/products/root/amd64_rhel50/5.34.00
+source setup_env.sh
+PATH=$PATH:`pwd`
