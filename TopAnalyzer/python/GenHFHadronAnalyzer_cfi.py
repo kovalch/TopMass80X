916676f635dfbd6624d2d4085c6423b829d4dd0e
Nazar <nazar.bartosik@gmail.com>
2014-07-18 00:50:18 +0200

    Removed the obsolete GenEvent input of the GenHFHadronMatcher module

--- TopAnalyzer/python/GenHFHadronAnalyzer_cfi.py
+++ TopAnalyzer/python/GenHFHadronAnalyzer_cfi.py
@@ -1,7 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 analyzeGenHFHadron = cms.EDAnalyzer("GenHFHadronAnalyzer",
-    ttGenEvent = cms.InputTag('genEvt'),
     genJets = cms.InputTag('ak5GenJetsPlusHadron','','SIM'),   
     flavour = cms.int32(5),
     onlyJetClusteredHadrons = cms.bool(False),
