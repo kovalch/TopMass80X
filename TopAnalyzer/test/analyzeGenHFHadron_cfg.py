3ac7824f26869c92a08c52f3f6707b13fc39035a
Nazar <nazar.bartosik@gmail.com>
2014-07-17 15:02:43 +0200

    Reduced the list of input files to get rid of the problematic one in the jet-hadron-quark testing analyzer

--- TopAnalyzer/test/analyzeGenHFHadron_cfg.py
+++ TopAnalyzer/test/analyzeGenHFHadron_cfg.py
@@ -17,17 +17,6 @@ process.MessageLogger.cerr.FwkReport.reportEvery = 100
 process.source = cms.Source("PoolSource",
    fileNames = cms.untracked.vstring(    
     ## add your favourite file here
-    '/store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v1/00000/FEF43CF5-E60F-E211-9FBB-E0CB4E1A117A.root',
-    '/store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v1/00000/FEB2FB74-2B10-E211-8B1F-00259073E322.root',
-    '/store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v1/00000/FEA9D489-1910-E211-A6E4-00259073E3D6.root',
-    '/store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v1/00000/FE675CF5-0810-E211-A5A3-E0CB4EA0A904.root',
-    '/store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v1/00000/FE64FE2E-DB0F-E211-BF12-90E6BA442F33.root',
-    '/store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v1/00000/FE1EAE1A-4410-E211-BBB4-001EC9D8A8D4.root',
-    '/store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v1/00000/FCE064CB-DB0F-E211-89C3-E0CB4EA0A937.root',
-    '/store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v1/00000/FCA17D81-0510-E211-A333-20CF305616CC.root',
-    '/store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v1/00000/FC8E9086-F10F-E211-9A3F-00259073E3B4.root',
-    '/store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v1/00000/FC742CD1-C00F-E211-8277-20CF305B0572.root',
-    '/store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v1/00000/FC354441-FF0F-E211-A9AC-E0CB4E553641.root',
     '/store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v1/00000/FC1E7E5A-E60F-E211-9605-485B39800C3B.root',
     '/store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v1/00000/FAB3BB25-2210-E211-968B-20CF305616E0.root',
     '/store/mc/Summer12_DR53X/TTJets_FullLeptMGDecays_8TeV-madgraph/AODSIM/PU_S10_START53_V7A-v1/00000/F8FDB54E-1110-E211-A162-E0CB4E19F962.root'
