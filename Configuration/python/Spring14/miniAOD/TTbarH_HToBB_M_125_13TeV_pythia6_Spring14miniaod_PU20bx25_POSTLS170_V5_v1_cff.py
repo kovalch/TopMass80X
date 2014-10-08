14cb7b8d60dd88e64fb7a302df516b04222f425e
Eleni Ntomari <Eleni.Ntomari@desy.de>
2014-10-08 09:40:51 +0200

    CSA14: more samples available

---
+++ Configuration/python/Spring14/miniAOD/TTbarH_HToBB_M_125_13TeV_pythia6_Spring14miniaod_PU20bx25_POSTLS170_V5_v1_cff.py
@@ -0,0 +1,14 @@
+import FWCore.ParameterSet.Config as cms
+
+maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+readFiles = cms.untracked.vstring()
+secFiles = cms.untracked.vstring() 
+source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
+readFiles.extend( [
+       '/store/mc/Spring14miniaod/TTbarH_HToBB_M-125_13TeV_pythia6/MINIAODSIM/PU20bx25_POSTLS170_V5-v1/00000/0E97DD3E-2209-E411-8A04-003048945312.root',
+       '/store/mc/Spring14miniaod/TTbarH_HToBB_M-125_13TeV_pythia6/MINIAODSIM/PU20bx25_POSTLS170_V5-v1/00000/D0242854-2209-E411-B361-003048C559CE.root' ] );
+
+
+secFiles.extend( [
+               ] )
+
