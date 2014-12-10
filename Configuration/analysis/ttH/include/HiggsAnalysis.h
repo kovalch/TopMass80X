0ba302825cc7dbf6b3a7be348503918b6a304dc3
Johannes <johannes.hauk@cern.ch>
2014-12-10 16:25:13 +0100

    Use gen b/c jet identification from AnalysisBase

--- Configuration/analysis/ttH/include/HiggsAnalysis.h
+++ Configuration/analysis/ttH/include/HiggsAnalysis.h
@@ -2,6 +2,7 @@
 #define HiggsAnalysis_h
 
 #include <Rtypes.h>
+#include <TString.h>
 
 class TTree;
 
@@ -84,41 +85,6 @@ private:
     
     
     
-    /// Returns a vector of indices of gen jets which are in acceptance
-    std::vector<int> genJetIndices(const VLV& allGenJets, const TopGenObjects& topGenObjects)const;
-    
-    /// Create vector of size of gen jets, and assign for each element a vector of indices of associated B hadrons
-    std::vector<std::vector<int> > matchBhadronsToGenJets(const std::vector<int>& genJetIndices, const VLV& allGenJets, 
-                                                          const TopGenObjects& topGenObjects)const;
-    
-    /// Create vector of size of gen jets, and assign for each element a vector of indices of associated C hadrons
-    std::vector<std::vector<int> > matchChadronsToGenJets(const std::vector<int>& genJetIndices, const VLV& allGenJets, 
-                                                          const TopGenObjects& topGenObjects)const;
-    
-    /// Returns vector of indices of gen jets containing B hadrons
-    std::vector<int> genBjetIndices(const std::vector<std::vector<int> >& genJetBhadronIndices)const;
-    
-    /// Returns vector of indices of gen jets containing C hadrons, but no B hadrons
-    std::vector<int> genCjetIndices(const std::vector<std::vector<int> >& genJetBhadronIndices,
-                                    const std::vector<std::vector<int> >& genJetChadronIndices)const;
-    
-    /// Get indices of generated (anti-)b jet by its mother particle ID ("signed" mother PDG ID, "+" is b, "-" is anti-b)
-    /// Requires that exactly one (anti-)b quark is found for the given ID
-    /// Returns index of corresponding gen jet, or negative value for indicating cases no jet could be identified
-    int genBjetIndex(const TopGenObjects& topGenObjects, const int pdgId)const;
-    
-    
-    /// Match generated jet with given index to reco jets
-    /// Returns the index of the matched reco jet, or negative value for indicating cases no jet could be identified
-    int matchRecoToGenJet(const std::vector<int>& jetIndices, const VLV& jets, const int genJetIndex, const VLV& genJets)const;
-    
-    /// Create vector of size of gen jets,
-    /// and assign for each element the index of the matched reco jet, or negative value for indicating cases no jet could be identified
-    std::vector<int> matchRecoToGenJets(const std::vector<int>& jetIndices, const VLV& jets,
-                                        const std::vector<int>& genJetIndices, const VLV& allGenJets)const;
-    
-    
-    
     /// Return vector of pair of indices for dijet combinations, each pair ordered by jet charge
     tth::IndexPairs chargeOrderedJetPairIndices(const std::vector<int>& jetIndices,
                                                 const std::vector<double>& jetCharges);
@@ -153,10 +119,6 @@ private:
     /// For a ttbar sample, select tt+bb, tt+b, or tt+other events (no separation for default value -999)
     int additionalBjetMode_;
 
-    /// Whether gen. jets that are close to leptons from tt decays should be removed
-    bool toRemoveLeptonGenJets_;
-    
-    
     /// Name of the reweighting function
     TString reweightingName_;
     
