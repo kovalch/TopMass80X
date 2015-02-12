a16c21081c0c1d4e13efde0d5808c4baa615ba93
Johannes <johannes.hauk@cern.ch>
2015-02-12 18:31:42 +0100

    Added functions to identify selection steps and jet categories

--- Configuration/analysis/ttH/include/higgsUtils.h
+++ Configuration/analysis/ttH/include/higgsUtils.h
@@ -25,6 +25,12 @@ namespace tth{
     
     
     
+    /// Assign a category name for one category, negative value means no category and returns empty string
+    TString categoryName(const int& category);
+    
+    /// Assign a category name for a set of categories
+    TString categoryName(const std::vector<int>& v_category);
+    
     /// Assign a step name for a given short name of the step, and potentially for a specific category
     TString stepName(const TString& stepShort, const int& category =-1);
     
@@ -42,13 +48,17 @@ namespace tth{
     /// Get from a TString the selection step of pattern "step*", combined with 0, 1 or several categories of pattern "cate*"
     TString extractSelectionStepAndJetCategory(const TString& name);
     
+    /// Get from a TString the number of categories of pattern "cate*"
+    int numberOfCategories(const TString& name);
+    
     /// Helper functions only needed by functions defined in this file
     namespace helper{
         
         /// Helper function to get the fragment containing a searchPattern,
         /// fragments are separated by given token,
         /// Flag allowMultiple to switch whether pattern can exist several times or only once
-        TString searchFragmentByToken(const TString& name, const TString& searchPattern, const TString& token, const bool allowMultiple =false);
+        TString searchFragmentByToken(const TString& name, const TString& searchPattern,
+                                      const TString& token, const bool allowMultiple =false);
     }
 }
 
