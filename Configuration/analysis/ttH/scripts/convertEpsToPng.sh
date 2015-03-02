57a7daf591c85f8f8716b1f77d257d1611a938aa
Jasone Garay <jasone.garay.garcia@cern.ch>
2015-03-02 18:07:46 +0100

    Add brief instructions about how to run the convertEpsToPng.sh script

--- Configuration/analysis/ttH/scripts/convertEpsToPng.sh
+++ Configuration/analysis/ttH/scripts/convertEpsToPng.sh
@@ -1,5 +1,9 @@
 #! /bin/sh
 
+# Run this script via (from TopAnalysis/Configuration/analysis/ttH) :
+# ./scripts/convertEpsToPng.sh <path to folder with eps files>
+
+
 InputFolder=$1
 
 cd $InputFolder
