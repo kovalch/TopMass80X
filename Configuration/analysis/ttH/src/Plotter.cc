88d4b7c0f86f53ba45ac84c1c3c1e37285ede431
Nazar Bartosik <nazar.bartosik@desy.de>
2015-05-12 19:47:09 +0200

    Recovered the default ratio-axis range

--- Configuration/analysis/ttH/src/Plotter.cc
+++ Configuration/analysis/ttH/src/Plotter.cc
@@ -398,7 +398,7 @@ void Plotter::write(const Channel::Channel& channel, const Systematic::Systemati
     for(TPaveText* label : significanceLabels) if(label) label->Draw("same");
     legend->Draw("SAME");
     if(dataHist.second && stacksum){
-        common::drawRatioPad(canvas, 0., 2.4, "#frac{Data}{MC}");
+        common::drawRatioPad(canvas, 0.5, 1.7, "#frac{Data}{MC}");
         
         TH1* ratio_histo = common::ratioHistogram(dataHist.second, stacksum, 1);
         common::setHistoStyle(ratio_histo, -1,-1,2);
