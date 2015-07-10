4a9febdd2849399193c8e158f4b5b425a7d77fe6
chrisjcc <christian.contreras@desy.de>
2015-07-10 14:10:25 +0200

    minor fix on the checking and creating of csv directory, done now more robustly

--- Configuration/analysis/ttH/scripts/mergeEventByEventInfo.sh
+++ Configuration/analysis/ttH/scripts/mergeEventByEventInfo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-############################################ 
+############################################
 #                                          #
 #   Write by: Christian Contreras-Campana  #
 #   email: christian.contreras@desy.de     #
@@ -10,7 +10,7 @@
 
 # Description: The following script merges the event-by-event information for synchronisation files based on whether if it's signal or background process
 
-# Useage: ./mergeEventByEventInfo.sh -p ttbarH125inclusive -s Nominal -c emu ( -o ttH_JESUp use only if -c is not called on)
+# Useage: ./install/bin/mergeEventByEventInfo.sh -p ttbarH125inclusive -s Nominal -c emu ( -o ttH_JESUp use only if -c is not called on)
 
 # Parses command line option
 function optparse {
@@ -18,7 +18,7 @@ function optparse {
     
     while getopts hvo:s:c:p:d OPT; do
 
-	case "$OPT" in
+        case "$OPT" in
             h)
 		echo $USAGE
 		exit 0
@@ -49,21 +49,22 @@ function optparse {
 		echo "Invalid option: -$OPTARG" >&2
 		exit 1
 		;;
-	esac
+        esac
     done
         
     # Remove the switches we parsed above.
     shift $((OPTIND-1))
 }
-# EOF
 
 
 # Function handles which event-by-event information files to merge
-function function_name () { 
-    
+function function_name () {
+
     # Create csv directory in current working directory
-    if [[ ! -d $cwd ]];then
-	cwd=`mkdir $PWD/csv ; cd csv ; echo "$PWD"`
+    if [[ ! -d $PWD/csv ]];then
+        cwd=`mkdir $PWD/csv; cd csv; echo "$PWD"`
+    else
+        cwd=`cd csv; echo "$PWD"`
     fi
 
     # Give local variables their corresponding global variable values
@@ -71,111 +72,111 @@ function function_name () {
     systematic="$SYSTEMATIC"
     channel="$CHANNEL"
     systematics=(Nominal JES_UP JES_DOWN)
-    
+
     # File hearder containing column labeling
     fileHeader="run,lumi,event,is_SL,is_DL,lep1_pt,lep1_eta,lep1_phi,lep1_iso,lep1_pdgId,lep2_pt,lep2_eta,lep2_phi,lep2_iso,lep2_pdgId,jet1_pt,jet2_pt,jet3_pt,jet4_pt,jet1_CSVv2,jet2_CSVv2,jet3_CSVv2,jet4_CSVv2,MET_pt,MET_phi,n_jets,n_btags,bWeight,ttHFCategory"
-    
+
     # If-statement selection is based on users input variables
     if [[ "${systematic}" != "*" && "${channel}" != "*" ]];then
 
-	# Merge based on specified systematic and channel type
-	dirPath="synchronisation/${systematic}/${channel}"
-	inFile="${systematic}_${channel}_${channel}_${process}*.csv"
-	outFile=$cwd/"${systematic}_${channel}_${channel}_${process}.csv"
-	
-	# Check if file(s) exist
-	countNumOfFiles=$(ls $dirPath/$inFile 2> /dev/null | wc -l)
+        # Merge based on specified systematic and channel type
+        dirPath="synchronisation/${systematic}/${channel}"
+        inFile="${systematic}_${channel}_${channel}_${process}*.csv"
+        outFile=$cwd/"${systematic}_${channel}_${channel}_${process}.csv"
 
-	if [[ "${countNumOfFiles}" == "0" ]];then
-	    printf "WARNING: The file(s) $dirPath/$inFile do not exist."
-	    exit
-	fi
+        # Check if file(s) exist
+        countNumOfFiles=$(ls $dirPath/$inFile 2> /dev/null | wc -l)
 
-	# Remove original file headers and empty lines and order info based on event number                                          
-	cat $dirPath/$inFile | sed -r "s/${fileHeader}//g" | sed '/^$/d' > $outFile
-	sed --in-place=.tmp "1s/^/${fileHeader}\n/" $outFile
-	cat $outFile | awk 'NR<2{print $0;next}{print $0| "sort --field-separator=',' -k 3 -n"}' > $outFile.old
-	mv $outFile.old $outFile
+        if [[ "${countNumOfFiles}" == "0" ]];then
+            printf "WARNING: The file(s) $dirPath/$inFile do not exist."
+            exit
+        fi
+
+        # Remove original file headers and empty lines and order info based on event number
+        cat $dirPath/$inFile | sed -r "s/${fileHeader}//g" | sed '/^$/d' > $outFile
+        sed --in-place=.tmp "1s/^/${fileHeader}\n/" $outFile
+        cat $outFile | awk 'NR<2{print $0;next}{print $0| "sort --field-separator=',' -k 3 -n"}' > $outFile.old
+        mv $outFile.old $outFile
 
     elif [[ "${systematic}" != "*" && "${channel}" == "*" ]];then
 
-	# Merge channel for based on systematic type
-	dirPath="synchronisation/${systematic}/${channel}"
-	inFile="${systematic}_*_*_${process}*.csv"
-	outFile=$cwd/"${systematic}_${process}.csv"
-	
-	# Check if file(s) exist                                                                                          
-	countNumOfFiles=$(ls $dirPath/$inFile 2> /dev/null | wc -l)
-	
-	if [[ "${countNumOfFiles}" == "0" ]];then
-	    prinf "WARNING: The file(s) $dirPath/$inFile do not exist."
-	    exit
-	fi
-	
-	# Remove original file headers and empty lines and order info based on event number
-	cat $dirPath/$inFile | sed -r "s/${fileHeader}//g" | sed '/^$/d' > $outFile
-	sed --in-place=.tmp "1s/^/${fileHeader}\n/" $outFile
-	cat $outFile | awk 'NR<2{print $0;next}{print $0| "sort --field-separator=',' -k 3 -n"}' > $outFile.old
+        # Merge channel for based on systematic type
+        dirPath="synchronisation/${systematic}/${channel}"
+        inFile="${systematic}_*_*_${process}*.csv"
+        outFile=$cwd/"${systematic}_${process}.csv"
 
-	if [[ ! -z $OUTPUT_FILE ]];then
-	    mv $outFile.old  $OUTPUT_FILE.csv
-	    rm $outFile
-	else
-	    mv $outFile.old $outFile
-	fi
+        # Check if file(s) exist
+        countNumOfFiles=$(ls $dirPath/$inFile 2> /dev/null | wc -l)
+
+        if [[ "${countNumOfFiles}" == "0" ]];then
+            prinf "WARNING: The file(s) $dirPath/$inFile do not exist."
+            exit
+        fi
+
+        # Remove original file headers and empty lines and order info based on event number
+        cat $dirPath/$inFile | sed -r "s/${fileHeader}//g" | sed '/^$/d' > $outFile
+        sed --in-place=.tmp "1s/^/${fileHeader}\n/" $outFile
+        cat $outFile | awk 'NR<2{print $0;next}{print $0| "sort --field-separator=',' -k 3 -n"}' > $outFile.old
+
+        if [[ ! -z $OUTPUT_FILE ]];then
+            mv $outFile.old  $cwd/$OUTPUT_FILE.csv
+            rm $outFile
+        else
+            mv $outFile.old $outFile
+        fi
 
     elif [[ "$systematic" == "*" && "$channel" != "*" ]];then
 
-	# Merge all systematic type based on channel type
-	for systematic in "${systematics[@]}" ; do
+        # Merge all systematic type based on channel type
+        for systematic in "${systematics[@]}" ; do
 
-	    dirPath="synchronisation/${systematic}/${channel}"
-	    inFile="${systematic}_${channel}_${channel}_${process}*.csv"
-	    outFile=$cwd/"${systematic}_${channel}_${channel}_${process}.csv"	    
+            dirPath="synchronisation/${systematic}/${channel}"
+            inFile="${systematic}_${channel}_${channel}_${process}*.csv"
+            outFile=$cwd/"${systematic}_${channel}_${channel}_${process}.csv"
 
-	    # Check if file(s) exist                                                                                         
-	    countNumOfFiles=$(ls $dirPath/$inFile 2> /dev/null | wc -l)
+            # Check if file(s) exist
+            countNumOfFiles=$(ls $dirPath/$inFile 2> /dev/null | wc -l)
 
-	    if [[ "${countNumOfFiles}" == "0" ]];then
-	        printf "WARNING: The file(s) $dirPath/$inFile do not exist."
-	        exit
-	    fi
+            if [[ "${countNumOfFiles}" == "0" ]];then
+                printf "WARNING: The file(s) $dirPath/$inFile do not exist."
+                exit
+            fi
 
-	    # Remove original file headers and empty lines and order info based on event number 
-	    cat $dirPath/$inFile | sed -r "s/${fileHeader}//g" | sed '/^$/d' > $outFile
-	    sed --in-place=.tmp "1s/^/${fileHeader}\n/" $outFile
-	    cat $outFile | awk 'NR<2{print $0;next}{print $0| "sort --field-separator=',' -k 3 -n"}' > $outFile.old
-	    mv $outFile.old $outFile
-	done
-	
+            # Remove original file headers and empty lines and order info based on event number
+            cat $dirPath/$inFile | sed -r "s/${fileHeader}//g" | sed '/^$/d' > $outFile
+            sed --in-place=.tmp "1s/^/${fileHeader}\n/" $outFile
+            cat $outFile | awk 'NR<2{print $0;next}{print $0| "sort --field-separator=',' -k 3 -n"}' > $outFile.old
+            mv $outFile.old $outFile
+        done
+
     elif [[ "${systematic}" == "*" && "${channel}" == "*" ]];then
 
-	# Merge separately over all systematic and channel type 
-	for systematic in "${systematics[@]}" ; do
+        # Merge separately over all systematic and channel type
+        for systematic in "${systematics[@]}" ; do
 
-	    dirPath="synchronisation/${systematic}/${channel}"
-	    inFile="${systematic}_*_*_${process}*.csv"
-	    outFile=$cwd/"${systematic}_${process}.csv"
+            dirPath="synchronisation/${systematic}/${channel}"
+            inFile="${systematic}_*_*_${process}*.csv"
+            outFile=$cwd/"${systematic}_${process}.csv"
 
-	    # Check if file(s) exist                                                                                         
-	    countNumOfFiles=$(ls $dirPath/$inFile 2> /dev/null | wc -l)
-	    
-	    if [[ "${countNumOfFiles}" == "0" ]];then
-	        prinf "WARNING: The file(s) $dirPath/$inFile do not exist."
-	        exit
-	    fi
-	    
-            # Remove original file headers and empty lines and order info based on event number 
-	    cat $dirPath/$inFile | sed -r "s/${fileHeader}//g" | sed '/^$/d' > $outFile 
-	    sed --in-place=.tmp "1s/^/${fileHeader}\n/" $outFile
-	    cat $outFile | awk 'NR<2{print $0;next}{print $0| "sort --field-separator=',' -k 3 -n"}' > $outFile.old
-	    mv $outFile.old $outFile	   
+            # Check if file(s) exist
+            countNumOfFiles=$(ls $dirPath/$inFile 2> /dev/null | wc -l)
+
+            if [[ "${countNumOfFiles}" == "0" ]];then
+                prinf "WARNING: The file(s) $dirPath/$inFile do not exist."
+                exit
+            fi
+
+            # Remove original file headers and empty lines and order info based on event number
+            cat $dirPath/$inFile | sed -r "s/${fileHeader}//g" | sed '/^$/d' > $outFile
+            sed --in-place=.tmp "1s/^/${fileHeader}\n/" $outFile
+            cat $outFile | awk 'NR<2{print $0;next}{print $0| "sort --field-separator=',' -k 3 -n"}' > $outFile.old
+            mv $outFile.old $outFile
         done
     fi
 }
 
 # Parse command line options
-optparse $@ 
+optparse $@
 
 # If variable is not definted then assign wildcard value
 SYSTEMATIC=$([ "${SYSTEMATIC}" == '' ] && echo "*" || echo "${SYSTEMATIC}")
@@ -189,10 +190,12 @@ if [[ -z "${PROCESS}" ]];then
 
     PROCESS="ttbarDilepton"
     function_name $PROCESS $SYSTEMATIC $CHANNEL
-    
+
 else
     function_name $PROCESS $SYSTEMATIC $CHANNEL
 fi
 
 # Remove tmp files
-rm $cwd/*.csv.tmp 
+rm $cwd/*.csv.tmp
+
+# EOF
