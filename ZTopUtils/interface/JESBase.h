e616bd0ae42291d8cd7137beed5a5e9d6415b40e
Johannes <johannes.hauk@cern.ch>
2015-07-16 10:32:01 +0200

    JESBase: Added functions for easier application of corrections and systematics

--- ZTopUtils/interface/JESBase.h
+++ ZTopUtils/interface/JESBase.h
@@ -29,88 +29,93 @@ namespace ztop {
 class JESBase {
 public:
 
-	JESBase() {
-		is2012_ = true;
-		totalunc_ = 0;
-		noupdown_=0;
-                correction_=1.0;
-	}
-	JESBase(const ztop::JESBase &);
-	JESBase & operator =(const ztop::JESBase &);
-	~JESBase();
+    JESBase() {
+        is2012_ = true;
+        totalunc_ = 0;
+        noupdown_=0;
+    }
+    JESBase(const ztop::JESBase &);
+    JESBase & operator =(const ztop::JESBase &);
+    ~JESBase();
 
-	void setFile(std::string pathToFile, bool quiet = false);
-	void setSystematics(std::string); //! up, down, no
-	void setIs2012(bool is) {
-		is2012_ = is;
-		std::cout << "JES mode changed; set File again!" << std::endl;
-	}
-
-
-	/**
-	 * ADDS! a source with name to the sources to be varied
-	 */
-	void setSource(const std::string&);
-	/**
-	 * resets configuration as far as sources to be varied are concerned
-	 */
-	void clearSources(){sources_.clear();}
+    void setFile(std::string pathToFile, bool quiet = false);
+    void setSystematics(std::string); //! up, down, no
+    void setIs2012(bool is) {
+        is2012_ = is;
+        std::cout << "JES mode changed; set File again!" << std::endl;
+    }
 
 
-	/**
-	 * returns a vector of available source names
-	 */
-	std::vector<std::string> getSourceNames()const;
+    /**
+     * ADDS! a source with name to the sources to be varied
+     */
+    void setSource(const std::string&);
+    
+    /**
+     * resets configuration as far as sources to be varied are concerned
+     */
+    void clearSources(){sources_.clear();}
 
-	/**
-	 * Applies uncertainties only if they match the flavour specified here
-	 * This function ADDS and entry! to clear, use clearRestrictToFlavour()
-	 */
-	void restrictToFlavour(int flav);
 
-	/**
-	 * Clears all flavour restrictions added before
-	 */
-	void clearRestrictToFlavour();
+    /**
+     * returns a vector of available source names
+     */
+    std::vector<std::string> getSourceNames()const;
 
-	/**
-	 * applies the uncertainties on jet quantities.
-	 * jetFlavour is the absolute of the one given by PAT! If none specified (<9998), uncertainties will be
-	 * applied to all in the same manner. (default for backward compatibility)
-	 * The same is true if no restriction is specified in restrictToAbsFlavour();
-	 */
-	void applyUncertainties(float & pt, float& eta, float & phi, float& m, int jetFlavour=-9999);
+    /**
+     * Applies uncertainties only if they match the flavour specified here
+     * This function ADDS and entry! to clear, use clearRestrictToFlavour()
+     */
+    void restrictToFlavour(int flav);
 
-        /**
-         * provides correction value due to JES, which should be used for pt, et correction of the particular uncorrected jet used
-         * as the input for this function
-         */
-        double correctionForUncorrectedJet(const double& jetInitialArea, const double& jetInitialEta, const double& jetInitialPt, const double& rho);
-        
-        /**
-         * sets files which will be used in getCorrectionValueForUncorrectedJet() for getting correction values due to JES,
-         * boolean "isMC" triggers usage of "L2L3Residual" JES corrections for Data
-         */
-        void configureFactorizedJetCorrector(const TString* filePathL1, const TString* filePathL2, const TString* filePathL3, const TString* filePathL2L3, const bool& isMC);
+    /**
+     * Clears all flavour restrictions added before
+     */
+    void clearRestrictToFlavour();
 
+    /**
+     * applies the uncertainties on jet quantities.
+     * jetFlavour is the absolute of the one given by PAT! If none specified (<9998), uncertainties will be
+     * applied to all in the same manner. (default for backward compatibility)
+     * The same is true if no restriction is specified in restrictToAbsFlavour();
+     */
+    void applyUncertainties(float & pt, float& eta, float & phi, float& m, int jetFlavour=-9999);
+    
+    /// Get systematic uncertainty factor to be directly multiplied with jet P4
+    double uncertaintyFactor(const double& pt, const double& eta, const int jetFlavour =-9999)const;
+    
+    /**
+     * provides correction value due to JES, which should be used for pt, et correction of the particular uncorrected jet used
+     * as the input for this function
+     */
+    double correctionForUncorrectedJet(const double& jetInitialArea, const double& jetInitialEta, const double& jetInitialPt, const double& rho);
+    
+    /**
+     * sets files which will be used in getCorrectionValueForUncorrectedJet() for getting correction values due to JES,
+     * boolean "isMC" triggers usage of "L2L3Residual" JES corrections for Data
+     */
+    void configureFactorizedJetCorrector(const TString* filePathL1, const TString* filePathL2, const TString* filePathL3, const TString* filePathL2L3, const bool& isMC);
+    
+    /// Configure factorised jet corrector from a vector of filenames, each representing one correction in the order of the vector
+    void configureFactorizedJetCorrector(const std::vector<std::string>& v_filename);
+    
 protected:
-	std::vector<unsigned int> & sources() {
-		return sources_;
-	}
-	std::string pathToFile_;
-	std::vector<JetCorrectionUncertainty*> vsrc_;
-	JetCorrectionUncertainty* totalunc_;
-	int noupdown_;
-	std::vector<unsigned int> sources_;
-	std::map<std::string,unsigned int> sourcenames_;
-	bool is2012_;
+    std::vector<unsigned int> & sources() {
+        return sources_;
+    }
+    std::string pathToFile_;
+    std::vector<JetCorrectionUncertainty*> vsrc_;
+    JetCorrectionUncertainty* totalunc_;
+    int noupdown_;
+    std::vector<unsigned int> sources_;
+    std::map<std::string,unsigned int> sourcenames_;
+    bool is2012_;
 
-	std::vector<int> restricttoflav_;
+    std::vector<int> restricttoflav_;
 
-	void copyFrom(const ztop::JESBase &);
+    void copyFrom(const ztop::JESBase &);
         
-        double correction_;
-        FactorizedJetCorrector *jetCorrector_;
+    FactorizedJetCorrector *jetCorrector_;
 
 };
 
