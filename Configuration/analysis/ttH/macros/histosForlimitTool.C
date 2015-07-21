b7f896819c4a94968737eeb0b7d9d7175918dbf5
chrisjcc <christian.contreras@desy.de>
2015-07-21 16:17:57 +0200

    upgrade to code: mapping between internal and external cms naming convention between systematic is done now by provinding an external configuration file (not hard coded anymore)

--- Configuration/analysis/ttH/macros/histosForlimitTool.C
+++ Configuration/analysis/ttH/macros/histosForlimitTool.C
@@ -25,7 +25,7 @@
 #include <string>
 #include <sstream>
 #include <typeinfo>
-
+#include <fstream>
 #include <map>
 
 #include "TSystem.h"
@@ -41,6 +41,7 @@
 #include <TObject.h>
 #include <TObjString.h>
 
+
 // Function handles producing root file with category, process, obserable, systematic mva information
 void histosForlimitTool(TString Str = "mvaEventA_mvaWeight", TString outputFileName = "mvaEventA_mvaWeight")
 {
@@ -50,43 +51,23 @@ void histosForlimitTool(TString Str = "mvaEventA_mvaWeight", TString outputFileN
   // Types of systematic variations mapping to naming convention of ttH collaboration
   std::map<TString,TString> mapOfSystematics;
 
-  mapOfSystematics["BTAGDISCR_BPURITY_DOWN"] = "CMS_CSVHF_Down";
-  mapOfSystematics["BTAGDISCR_BPURITY_UP"]   = "CMS_CSVHF_Up";
-  mapOfSystematics["BTAGDISCR_BSTAT2_DOWN"]  = "CMS_CSVHFStats2_Down";
-  mapOfSystematics["BTAGDISCR_BSTAT2_UP"]    = "CMS_CSVHFStats2_Up";
-  mapOfSystematics["BTAGDISCR_CERR2_DOWN"]   = "CMS_CSVCErr2_Down";
-  mapOfSystematics["BTAGDISCR_CERR2_UP"]     = "CMS_CSVCErr2_Down";
-  mapOfSystematics["BTAGDISCR_LSTAT1_DOWN"]  = "CMS_CSVLFStats1_Down";
-  mapOfSystematics["BTAGDISCR_LSTAT1_UP"]    = "CMS_CSVLFStats1_Up";
-  mapOfSystematics["BTAGDISCR_BSTAT1_DOWN"]  = "CMS_CSVHFStats1_Down";
-  mapOfSystematics["BTAGDISCR_BSTAT1_UP"]    = "CMS_CSVHFStats1_Up";
-  mapOfSystematics["BTAGDISCR_CERR1_DOWN"]   = "CMS_CSVCErr1_Down";
-  mapOfSystematics["BTAGDISCR_CERR1_UP"]     = "CMS_CSVCErr1_Up";
-  mapOfSystematics["BTAGDISCR_LPURITY_DOWN"] = "CMS_CSVLF_Down";
-  mapOfSystematics["BTAGDISCR_LPURITY_UP"]   = "CMS_CSVLF_Up";
-  mapOfSystematics["BTAGDISCR_LSTAT2_DOWN"]  = "CMS_CSVLFStats2_Down";
-  mapOfSystematics["BTAGDISCR_LSTAT2_UP"]    = "CMS_CSVLFStats2_Up";
-  mapOfSystematics["JES_DOWN"] = "scale_j_Down";
-  mapOfSystematics["JES_UP"]   = "scale_j_Up";
-  mapOfSystematics["Nominal"]  = "Nominal";
+  std::string internalLable,;
+  std::string externalLabel;
+  
+  TString labelString;
+  TObjString labels;
 
-  // Additional systematics not included for the Baseline analysis
-  mapOfSystematics["XSEC_TT2B_UP"]    = "CMS_XSEC_TT2B_Up";
-  mapOfSystematics["XSEC_TT2B_DOWN"]  = "CMS_XSEC_TT2B_Down";
-  mapOfSystematics["XSEC_TTCC_UP"]    = "CMS_XSEC_TTCC_Up";
-  mapOfSystematics["XSEC_TTCC_DOWN"]  = "CMS_XSEC_TTCC_Down";
-  mapOfSystematics["XSEC_TTH_UP"]     = "CMS_XSEC_TTH_Up";
-  mapOfSystematics["XSEC_TTH_DOWN"]   = "CMS_XSEC_TTH_Down";
-  mapOfSystematics["XSEC_TTOTHER_UP"] = "CMS_XSEC_TTOTHER_Up";
-  mapOfSystematics["XSEC_TTOTHER_DOWN"] = "CMS_TTOTHER_Down";
-  mapOfSystematics["XSEC_TTZ_UP"]       = "CMS_XSEC_TTZ_Up";
-  mapOfSystematics["XSEC_TTZ_DOWN"]     = "CMS_XSEC_TTZ_Down";
-  mapOfSystematics["FRAC_TTHF_UP"]      = "CMS_FRAC_TTHF_Up";
-  mapOfSystematics["FRAC_TTHF_DOWN"]    = "CMS_FRAC_TTHF_Down";
-  mapOfSystematics["FRAC_TTOTHER_UP"]   = "CMS_FRAC_TTOTHER_Up";
-  mapOfSystematics["FRAC_TTOTHER_DOWN"] = "CMS_FRAC_TTOTHER_Down";
-  mapOfSystematics["LUMI_UP"]   = "CMS_LUMI_Up";
-  mapOfSystematics["LUMI_DOWN"] = "CMS_LUMI_Down";
+  std::ifstream  mapConfig;
+  mapConfig.open("macros/EXTERNAL.txt",std::ios::in); 
+
+  // Fill maps and meta information
+  while(mapConfig >> internalLable >> externalLabel) {
+
+    if(!internalLable.empty() && !externalLabel.empty()) {
+      mapOfSystematics[internalLable] = externalLabel;
+      labelString  += internalLable+" "+externalLabel+"\n";
+    }
+  }
 
   // Event categories based on the # of jets and # of b-tagged jets
   std::map<TString,TString> mapOfCategories;
@@ -113,28 +94,28 @@ void histosForlimitTool(TString Str = "mvaEventA_mvaWeight", TString outputFileN
   struct dirent *entry2;
   struct dirent *entry3;
 
-  // Begin iterating throug the Plots directory for File System hierarchy
+  // Begin iterating through the Plots directory for File System hierarchy
   if((pDIR1=opendir("./Plots"))){
 
     while((entry1 = readdir(pDIR1))){
-
+      
       if( strcmp(entry1->d_name, ".") != 0 && strcmp(entry1->d_name, "..") != 0 ) {
 	//std::cout << entry1->d_name << "\n";
-
+	
 	if((pDIR2=opendir(std::string("./Plots/"+std::string(entry1->d_name)).c_str()))) {
-
+	  
 	  while((entry2 = readdir(pDIR2))) {
-
+	    
 	    if( strcmp(entry2->d_name, ".") != 0 && strcmp(entry2->d_name, "..") != 0 ) {
 	      //std::cout << entry2->d_name << "\n";
-
+	      
 	      if((pDIR3=opendir(std::string("./Plots/"+std::string(entry1->d_name)+"/"+std::string(entry2->d_name)).c_str()))) {
-
+		
 		while((entry3 = readdir(pDIR3))){
 		  //std::cout << entry3->d_name << "\n";
-
+		  
 		  TPRegexp subStr = TPRegexp(regexString+".root");
-
+		  
 		  // check we access only root files of a particular type
 		  if(TString(entry3->d_name).Contains(subStr)) {
 		    
@@ -147,35 +128,40 @@ void histosForlimitTool(TString Str = "mvaEventA_mvaWeight", TString outputFileN
 		    const TString cate = ((TObjString *)subStrArray->At(3))->GetString();
 
 		    TString observableType = "observable";
-
+		    
 		    // Store results in the macros directory
 		    const TString outpath("./macros");                 		    
-
+		    
 		    // Create input file handler
 		    TFile* sampleFile = new TFile(TString("./Plots/")+TString(entry1->d_name)+"/"+TString(entry2->d_name)+"/"+fileName);
-
+		    
 		    std::map<TString,TH1D*> mapOfHistograms;
-
+		    
 		    // Check that file exists and is not corrupt
 		    if(sampleFile->IsZombie()) {
 		      std::cout<<"\n\tWe didn't find the "+TString(entry1->d_name)+" input!!\n";
 		    }
-		    else {		      
+		    else {
+		      
 		      TString sample = (fileName.Copy()).ReplaceAll("_source.root","");
 		      
 		      // Update (i.e. append) to output file                                                                                           
 		      outputFile = new TFile(outpath+"/"+outputFileName+".root","UPDATE");
 		      
+		      outputFile->cd();
+		      labels = labelString.Data();
+		      labels.Write("MetaInfo",TObject::kOverwrite);
+
 		      // Create list from list of histogram from root file
 		      TList* list = sampleFile->GetListOfKeys();
-
+		      
 		      if(false)list->Print();
-
+		      
 		      if(!list) {
 			std::cout << TString::Format("<Error> No keys found in file\n");
 			exit(1);
 		      }
-
+		      
 		      // Create iterator from list
 		      TIter next((TList*)list);
 		      TKey* key;
@@ -186,21 +172,21 @@ void histosForlimitTool(TString Str = "mvaEventA_mvaWeight", TString outputFileN
 			// String capture of process type
 			TObjArray *subStrA = TPRegexp(regexString+".*").MatchS(TString(key->GetName()));
 			const TString processType  = ((TObjString *)subStrA->At(4))->GetString();
-
+			
 			// Store only histograms not containing "canvas" label
 			if (!processType.Contains("canvas"))
 			  mapOfHistograms[processType]     = (TH1D*)sampleFile->Get(key->GetName());
 		      }
-
+		      
 		      // Check histogram exist
 		      for (std::map<TString,TH1D*>::iterator iter = mapOfHistograms.begin(); iter != mapOfHistograms.end(); ++iter) {
 			if(!mapOfHistograms[iter->first])
-			std::cout << "\n\tWe didn't find the "+iter->first+" histogram!!\n";
+			  std::cout << "\n\tWe didn't find the "+iter->first+" histogram!!\n";
 		      }
-
+		      
 		      // Access integral values to determine observation and rates
 		      if (false) {
-
+			
 			std::cout << "File type: " << sample.Data() << std::endl;
 			std::cout << TString::Format("The integral for data is %f\n", mapOfHistograms["data"]->Integral());
 			std::cout << TString::Format("The integral for bkg is %f\n", mapOfHistograms["bkg"]->Integral());
@@ -222,8 +208,8 @@ void histosForlimitTool(TString Str = "mvaEventA_mvaWeight", TString outputFileN
 			TString label = TString(entry1->d_name).Contains("Nominal") ? iter->first : iter->first+"__"+mapOfSystematics[entry1->d_name];
 			mapOfHistograms[iter->first]->Write(label,TObject::kOverwrite);
                       }
-
-		      outputFile->Write("",TObject::kOverwrite);
+		      		      
+		      outputFile->Write("",TObject::kOverwrite);		   
 		      outputFile->Close();		    
 		    } // End section where histograms are written
 		    
