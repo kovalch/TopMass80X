74905ce15af9949f2cd9166d3ce8f1a95d3b43b8
Hartmut Stadie <hartmut.stadie@desy.de>
2015-12-08 11:25:55 +0100

    use clang format(in ecplipse)

--- Analyzer/root/TemplateDerivation_main.cc
+++ Analyzer/root/TemplateDerivation_main.cc
@@ -1,10 +1,9 @@
 #include "ProgramOptionsReader.h"
 #include "TemplateDerivation.h"
 
-int main(int ac, char** av)
-{
-  ProgramOptionsReader por = ProgramOptionsReader(ac,av);
+int main(int ac, char** av) {
+  ProgramOptionsReader por = ProgramOptionsReader(ac, av);
 
-  TemplateDerivation *top = new TemplateDerivation();
+  TemplateDerivation* top = new TemplateDerivation();
   delete top;
 }
