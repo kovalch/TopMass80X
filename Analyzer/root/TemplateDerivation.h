81c537f1a45a828c8835ae9a56cd5ace994089a5
Hartmut Stadie <hartmut.stadie@desy.de>
2015-12-14 16:36:50 +0100

    TemplateDerivation: split code in functions and first unit test

--- Analyzer/root/TemplateDerivation.h
+++ Analyzer/root/TemplateDerivation.h
@@ -5,9 +5,14 @@
 #include "TChain.h"
 #include "TH2F.h"
 
-#include "RooFormulaVar.h"
-
 #include <string>
+
+class RooWorkspace;
+class RooDataSet;
+class RooArgSet;
+class RooRealVar;
+class RooFormulaVar;
+class RooFitResult;
 
 class TemplateDerivation {
  public:
@@ -21,6 +26,9 @@ class TemplateDerivation {
   const int maxPermutations_;
   const double maxMtop_;
 
+  std::vector<double> jesValues_;
+  std::vector<double> massValues_;
+
   enum channelID {
     kAllJets,
     kMuonJets,
@@ -29,27 +37,33 @@ class TemplateDerivation {
     kMaxChannels
   };
   int channelID_;
-
-  bool doCalibration_;
-  bool fitBackground_;
+  RooWorkspace* workspace_;
 
   // do all the calculation
-  void rooFitTopMass_();
+  void rooFitTopMass();
 
   // because RooFit only likes plain trees with standard data types (int, float,
   // double, ...)
   // the original tree has to be adapted for the new content
-  TTree* modifiedTree_(TChain* tree, int comboType);
-  TTree* modifiedTree_(TChain* tree, int minComboType = -10,
-                       int maxComboType = 10, bool isData = false);
+  TTree* modifiedTree(TChain* tree, int minComboType = -10,
+                      int maxComboType = 10, bool isData = false);
   void UnknownChannelAbort();
 
+  void addTemplateFunction(int templType, int comboType, int nComboTypes,
+                           RooRealVar* mTop, RooRealVar* JES);
+  RooFitResult* fitTemplate(int templType, int comboType, int nComboTypes);
+  void plotResult(int templType, int comboType, int nComboTypes);
+  void printResult(int templType, int comboType, int nComboTypes);
   void fillAlpha(std::vector<RooFormulaVar*>& alpha, int& h, RooArgSet argSet,
                  std::string add = "");
-  std::vector<RooDataSet*> createDataSets(std::vector<double> masses,
-                                          std::vector<double> JESes,
-                                          const RooArgSet& varSet);
+
+ public:
+  std::vector<RooDataSet*> createDataSets(const RooArgSet& varSet);
   std::string constructFileName(double mass, double jes);
+  static std::string constructTemplateName(double mass, double jes);
+  unsigned int numVariables(TString startName) const;
+  static TString constructName(const std::string& name, int i);
+  static TString constructName(const std::string& name, int i, int j);
 };
 
 #endif /* TEMPLATE_DERIVATION_H */
---
+++ Analyzer/tests/TemplateDerivation_test.cc
@@ -0,0 +1,21 @@
+/*
+ * TemplateDerivation_test.cc
+ *
+ *  Created on: 10.12.2015
+ *      Author: stadie
+ */
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE TemplateDerivationTest
+#include <boost/test/unit_test.hpp>
+#include "TemplateDerivation.h"
+
+BOOST_AUTO_TEST_CASE(template_derivation_test_TemplateName) {
+  BOOST_CHECK_EQUAL(TemplateDerivation::constructTemplateName(172.5, 1.02),
+                    "jes102mass1725");
+  BOOST_CHECK_EQUAL(TemplateDerivation::constructTemplateName(168.0, 0.98),
+                    "jes098mass1680");
+  BOOST_CHECK_EQUAL(
+      TemplateDerivation::constructTemplateName(166.5, 0.95999999),
+      "jes096mass1665");
+}
